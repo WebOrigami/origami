3e9b6a5ade4e8bd3192f4ba5ba07cdba41bec2cf
Jan Miksovsky <jan@miksovsky.com>
2020-11-24 10:27:34 -0800

    Reorg files. Experiment with static constructor call as a factory.

---
+++ demo/more/constructorAsFactory.js
@@ -0,0 +1,16 @@
+function Foo(obj) {
+  const name = obj?.name ?? "world";
+  if (!(this instanceof Foo)) {
+    return new Foo({ name });
+  } else {
+    this.name = name;
+  }
+}
+
+const constructed1 = Foo();
+const constructed2 = Foo({ name: "Alice" });
+console.log(constructed1, constructed2);
+
+const newed1 = new Foo();
+const newed2 = new Foo({ name: "Bob" });
+console.log(newed1, newed2);
