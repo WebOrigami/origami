6cfae5bab34754a91f66b8e79a90866e4981d980
Jan Miksovsky <jan@miksovsky.com>
2020-11-24 16:07:50 -0800

    Add symbols to Explorable exports too for convenience.

--- packages/exfn/exports.js
+++ packages/exfn/exports.js
@@ -1,4 +1,5 @@
 // Should generate this export file with explorable graphs!
+export { asyncGet, asyncKeys, get, keys } from "@explorablegraph/symbols";
 export { default as AsyncExplorable } from "./src/AsyncExplorable.js";
 export { default as AsyncExplorableObject } from "./src/AsyncExplorableObject.js";
 export { default as Explorable } from "./src/Explorable.js";
