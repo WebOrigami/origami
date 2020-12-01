37486f1829941fd812c52f4008b2e14c08749898
Jan Miksovsky <jan@miksovsky.com>
2020-11-30 17:55:51 -0800

    More tests.

--- packages/core/test/explorablePlainObject.tests.js
+++ packages/core/test/explorablePlainObject.tests.js
@@ -2,7 +2,7 @@ import { asyncGet, get } from "@explorablegraph/symbols";
 import chai from "chai";
 import AsyncExplorable from "../src/AsyncExplorable.js";
 import Explorable from "../src/Explorable.js";
-import explorableBuiltIn from "../src/explorablePlainObject.js";
+import explorablePlainObject from "../src/explorablePlainObject.js";
 const { assert } = chai;
 
 describe("explorablePlainObject", () => {
@@ -11,33 +11,37 @@ describe("explorablePlainObject", () => {
       a: 1,
       b: 2,
       c: 3,
+      more: {
+        d: 4,
+        e: 5,
+      },
     };
-    const obj = new explorableBuiltIn(original);
+    const obj = new explorablePlainObject(original);
     assert(obj instanceof Explorable);
     assert.equal(obj[get]("a"), 1);
     assert.equal(obj[get]("b"), 2);
     assert.equal(obj[get]("c"), 3);
     assert.equal(obj[get]("x"), undefined);
-    assert.deepEqual([...obj], ["a", "b", "c"]);
+    assert.deepEqual([...obj], ["a", "b", "c", "more"]);
 
     // Changes to original object are visible through get/keys.
-    original.a = 4;
-    original.d = 5;
-    assert.equal(obj[get]("a"), 4);
+    original.a = 6;
+    original.f = 7;
+    assert.equal(obj[get]("a"), 6);
     assert.equal(obj[get]("b"), 2);
     assert.equal(obj[get]("c"), 3);
-    assert.equal(obj[get]("d"), 5);
+    assert.equal(obj[get]("f"), 7);
     assert.equal(obj[get]("x"), undefined);
-    assert.deepEqual([...obj], ["a", "b", "c", "d"]);
+    assert.deepEqual([...obj], ["a", "b", "c", "more", "f"]);
 
     // Changes applied to the proxy are also represented.
-    obj.e = 6;
-    assert.equal(obj[get]("e"), 6);
-    assert.deepEqual([...obj], ["a", "b", "c", "d", "e"]);
+    obj.g = 8;
+    assert.equal(obj[get]("g"), 8);
+    assert.deepEqual([...obj], ["a", "b", "c", "more", "f", "g"]);
   });
 
   it("can async explore a plain JavaScript object", async () => {
-    const obj = new explorableBuiltIn({
+    const obj = new explorablePlainObject({
       a: 1,
       b: 2,
       c: 3,
