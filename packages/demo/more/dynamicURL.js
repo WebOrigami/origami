771a084ce4561a3269acb49f066a5fd28783203e
Jan Miksovsky <jan@miksovsky.com>
2021-03-22 10:03:34 -0700

    Tinkering

---
+++ packages/demo/more/dynamicURL.js
@@ -0,0 +1,11 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const filePath = path.resolve(dirname, "greet.js");
+const buffer = await fs.readFile(filePath);
+const url = `data:text/javascript,${buffer}`;
+// Alas, the imported JS can't include relative imports.
+const { default: result } = await import(url);
+console.log(result);
