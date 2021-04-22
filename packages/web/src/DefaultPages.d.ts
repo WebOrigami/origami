d379b4aaaabc230348df216bf39a0e1d5a3a557f
Jan Miksovsky <jan@miksovsky.com>
2021-04-22 11:51:07 -0700

    Type fix

---
+++ packages/web/src/DefaultPages.d.ts
@@ -0,0 +1,6 @@
+// import { ExplorableGraph } from "@explorablegraph/core";
+import ExplorableGraph from "@explorablegraph/core/src/ExplorableGraph.js";
+
+export default class DefaultPages extends ExplorableGraph {
+  constructor(inner: any);
+}
