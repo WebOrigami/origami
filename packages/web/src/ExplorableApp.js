dd8dd133db34663b32c71d98d78b181a3993f8f7
Jan Miksovsky <jan@miksovsky.com>
2021-05-10 11:00:30 -0700

    ExplorableApp disables DefaultPagesMixin for now while we work out a way to reconcile that behavior with WildcardKeysMixin.

--- packages/web/src/ExplorableApp.js
+++ packages/web/src/ExplorableApp.js
@@ -4,20 +4,24 @@ import {
   VirtualKeysMixin,
   VirtualValuesMixin,
 } from "../../node/exports.js";
-import DefaultPagesMixin from "./DefaultPagesMixin.js";
+// import DefaultPagesMixin from "./DefaultPagesMixin.js";
 
 // DefaultPagesMixin(Files) so that DefaultPagesMixin can respect real index.html
 
 // DefaultPagesMixin(VirtualKeys(...)) so that VirtualKeys can provide keys to index.html
 
-// WildcardGraph(DefaultPagesMixin(...)) so that DefaultPagesMixin can provide index.html
+// WildcardKeysMixin(DefaultPagesMixin(...)) so that DefaultPagesMixin can provide index.html
 // without triggering :notFound wildcard
+
+// BUT
+// DefaultPagesMixin(WildcardKeysMixin(...)) so that WildcardKeysMixin can
+// define an wildcard like :products/index.html.
 
 // DefaultPagesMixin(VirtualFiles(...)) so that VirtualFiles can generate a dynamic index.html
 
-// WildcardGraph(VirtualFiles(...)) so that VirtualFiles can generate a wildcard function
-// that WildcardGraph can resolve
+// WildcardKeysMixin(VirtualFiles(...)) so that VirtualFiles can generate a wildcard function
+// that WildcardKeysMixin can resolve
 
 export default class ExplorableApp extends WildcardKeysMixin(
-  DefaultPagesMixin(VirtualKeysMixin(VirtualValuesMixin(Files)))
+  VirtualKeysMixin(VirtualValuesMixin(Files))
 ) {}
