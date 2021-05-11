dab80d29aeb0984781dbf6240e2aa385f695862d
Jan Miksovsky <jan@miksovsky.com>
2021-05-11 14:43:50 -0700

    VirtualKeysMixin returns .keys.json virtual keys before any remaining actual keys.

--- packages/node/src/VirtualKeysMixin.js
+++ packages/node/src/VirtualKeysMixin.js
@@ -2,28 +2,46 @@ const keysJsonKey = ".keys.json";
 
 export default function VirtualKeysMixin(Base) {
   return class VirtualKeys extends Base {
+    #virtualKeys;
+
     async *[Symbol.asyncIterator]() {
-      // Yield the parent graph's keys.
-      yield* super[Symbol.asyncIterator]();
+      if (this.#virtualKeys === undefined) {
+        // See if we have a .keys.json value.
+        // REVIEW: We call super.get instead of this.get here. Otherwise, if we
+        // use this mixin with WildcardKeysMixin, we can end up in a loop:
+        // WildcardKeysMixin will want to get the keys, which will cause
+        // VirtualKeysMixin to look for a .keys.json file, which would ask
+        // WildcardKeysMixin to look for a file and (if it doesn't find one), look
+        // through its keys for a wildcard -- resulting in a loop.
+        const value = await super.get(keysJsonKey);
 
-      // See if we have a .keys.json value.
-      // REVIEW: We call super.get instead of this.get here. Otherwise, if we
-      // use this mixin with WildcardKeysMixin, we can end up in a loop:
-      // WildcardKeysMixin will want to get the keys, which will cause
-      // VirtualKeysMixin to look for a .keys.json file, which would ask
-      // WildcardKeysMixin to look for a file and (if it doesn't find one), look
-      // through its keys for a wildcard -- resulting in a loop.
-      const value = await super.get(keysJsonKey);
-
-      if (value !== undefined) {
-        // Yield the value (which should be an array) as keys.
-        const data =
-          value instanceof Function ?
-            await value.call(this) :
-            value instanceof Buffer || value instanceof String
+        if (value === undefined) {
+          // No virtual keys
+          this.#virtualKeys = [];
+        } else {
+          // Virtual keys.
+          this.#virtualKeys =
+            value instanceof Function
+              ? await value.call(this)
+              : value instanceof Buffer || value instanceof String
               ? JSON.parse(String(value))
               : value;
-        yield* data;
+        }
+      }
+
+      // Yield the virtual keys, keeping track of what they are.
+      const set = new Set();
+      for (const key of this.#virtualKeys) {
+        set.add(key);
+        yield key;
+      }
+
+      // Yield any additional keys in the graph, skipping keys that appeared in
+      // the virtual keys.
+      for await (const key of super[Symbol.asyncIterator]()) {
+        if (!set.has(key)) {
+          yield key;
+        }
       }
     }
   };
--- packages/node/test/VirtualKeysMixin.tests.js
+++ packages/node/test/VirtualKeysMixin.tests.js
@@ -1,6 +1,7 @@
 import chai from "chai";
 import path from "path";
 import { fileURLToPath } from "url";
+import { ExplorableObject } from "../../core/exports.js";
 import Files from "../src/Files.js";
 import VirtualKeysMixin from "../src/VirtualKeysMixin.js";
 const { assert } = chai;
@@ -12,9 +13,27 @@ const virtualKeysFolder = path.join(fixturesDirectory, "virtualKeys");
 class VirtualKeysFiles extends VirtualKeysMixin(Files) {}
 
 describe("VirtualKeysMixin", () => {
+  it("yields keys from .keys.json before other keys", async () => {
+    const graph = new (VirtualKeysMixin(ExplorableObject))({
+      a: 1,
+      b: 2,
+      c: 3,
+      e: 5,
+      ".keys.json": ["d", "c", "b", "a"],
+    });
+    assert.deepEqual(await graph.keys(), [
+      "d",
+      "c",
+      "b",
+      "a",
+      "e",
+      ".keys.json",
+    ]);
+  });
+
   it("can load keys from a .keys.json value", async () => {
     const virtualKeys = new VirtualKeysFiles(virtualKeysFolder);
     const keys = await virtualKeys.keys();
-    assert.deepEqual(keys, [".keys.json", "a", "b", "c"]);
+    assert.deepEqual(keys, ["a", "b", "c", ".keys.json"]);
   });
 });
