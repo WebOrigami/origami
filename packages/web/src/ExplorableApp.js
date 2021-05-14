87c074c22520b6156e5b30a30e9217182d47e8c3
Jan Miksovsky <jan@miksovsky.com>
2021-05-14 14:17:46 -0700

    Factor default index.html page into its own module.

--- packages/web/src/ExplorableApp.js
+++ packages/web/src/ExplorableApp.js
@@ -1,13 +1,10 @@
-import {
-  DefaultValues,
-  ExplorableGraph,
-  WildcardKeysMixin,
-} from "../../core/exports.js";
+import { DefaultValues, WildcardKeysMixin } from "../../core/exports.js";
 import {
   Files,
   VirtualKeysMixin,
   VirtualValuesMixin,
 } from "../../node/exports.js";
+import defaultIndexHtml from "./defaultIndexHtml.js";
 
 class AppGraph extends WildcardKeysMixin(
   VirtualKeysMixin(VirtualValuesMixin(Files))
@@ -21,24 +18,4 @@ export default class ExplorableApp extends DefaultValues {
     };
     super(main, defaults);
   }
-}
-
-async function defaultIndexHtml() {
-  const links = [];
-  for await (const key of this) {
-    // Skip keys that start with a "." (like .keys.json) or a ":" (wildcard
-    // character). Also skip adding a link to the index.html page itself.
-    if (!key.startsWith(".") && !key.startsWith(":") && key !== "index.html") {
-      const value = await this.get(key);
-      const href = value instanceof ExplorableGraph ? `${key}/` : key;
-      const link = `<li><a href="${href}">${href}</a></li>`;
-      links.push(link);
-    }
-  }
-  const heading = this.path ? `${this.path}` : `Index`;
-  const list = `
-    <h1>${heading}</h1>
-    <ul>\n${links.join("\n")}\n</ul>
-  `;
-  return list;
 }
