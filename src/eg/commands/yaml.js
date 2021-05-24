11a04c1b1830d94d4a5dfb3d3f5691dddbefc00a
Jan Miksovsky <jan@miksovsky.com>
2021-05-23 20:59:22 -0700

    Smarter yaml command

--- src/eg/commands/yaml.js
+++ src/eg/commands/yaml.js
@@ -1,9 +1,12 @@
 import YAML from "yaml";
+import { isPlainObject } from "../../core/utilities.js";
 
 export default async function yaml(graph) {
-  const obj = await graph.strings();
-  // const obj = await graph.plain();
-  const text = YAML.stringify(obj);
+  // Leave objects/arrays as is, but stringify other types.
+  const plain = await graph.mapValues((value) =>
+    isPlainObject(value) || value instanceof Array ? value : value?.toString?.()
+  );
+  const text = YAML.stringify(plain);
   return text;
 }
 
