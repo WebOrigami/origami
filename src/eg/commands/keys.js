da2ee1a52bb0b2dfcf406b97b968479ad3dbca1a
Jan Miksovsky <jan@miksovsky.com>
2021-05-25 10:53:50 -0700

    Fix reference to keys function.

--- src/eg/commands/keys.js
+++ src/eg/commands/keys.js
@@ -1,5 +1,7 @@
+import ExplorableGraph from "../../core/ExplorableGraph.js";
+
 export default async function keys(graph) {
-  return await graph.keys();
+  return await ExplorableGraph.keys(graph);
 }
 
 keys.usage = `keys(graph)\tThe top-level keys in the graph`;
