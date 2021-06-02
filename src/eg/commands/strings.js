f7edd4c0ea665a33832e2ff5c41dd43393b767a6
Jan Miksovsky <jan@miksovsky.com>
2021-06-02 12:03:09 -0700

    Restore eg strings command

--- src/eg/commands/strings.js
+++ src/eg/commands/strings.js
@@ -1,5 +1,7 @@
+import ExplorableGraph from "../../core/ExplorableGraph.js";
+
 export default async function strings(graph) {
-  return await graph.strings();
+  return await ExplorableGraph.strings(graph);
 }
 
 strings.usage = `strings(graph)\tCast both the keys and values of the graph to strings`;
