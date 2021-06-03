27e00631b9b37a416a1c2b4ba16c759a76a6191a
Jan Miksovsky <jan@miksovsky.com>
2021-06-03 15:22:06 -0700

    Drop eg structure command

--- src/eg/commands/structure.js
+++
@@ -1,5 +0,0 @@
-export default async function structure(graph) {
-  return await graph.structure();
-}
-
-structure.usage = `structure(graph)\tA plain object with the structure of the graph but null values`;
