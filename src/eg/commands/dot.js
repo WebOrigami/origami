8efa83448191d2fab5643778a11fe395fe2241c7
Jan Miksovsky <jan@miksovsky.com>
2021-06-11 14:58:50 -0700

    dot command renders edge labels

--- src/eg/commands/dot.js
+++ src/eg/commands/dot.js
@@ -1,9 +1,13 @@
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 
-export default async function dot(graph, rootLabel = "") {
+export default async function dot(graph) {
   const graphArcs = await statements(graph, "", "/");
+  //   node [label=""; shape=circle; width=0.25];
   return `digraph g {
   rankdir=LR;
+  node [shape=none];
+  edge [arrowhead=vee; arrowsize=0.75];
+
 ${graphArcs.join("\n")}
 }`;
 }
@@ -11,11 +15,12 @@ ${graphArcs.join("\n")}
 async function statements(graph, nodePath, nodeLabel) {
   let result = [];
 
-  result.push(`  "${nodePath}" [label="${nodeLabel}"];`);
+  // result.push(`  "${nodePath}" [label="${nodeLabel}"];`);
+  result.push(`  "${nodePath}" [label=""; shape=circle; width=0.10];`);
 
   for await (const key of graph) {
     const destPath = `${nodePath}/${key}`;
-    const arc = `  "${nodePath}" -> "${destPath}";`;
+    const arc = `  "${nodePath}" -> "${destPath}" [label="${key}"];`;
     result.push(arc);
 
     const value = await graph.get(key);
@@ -23,7 +28,13 @@ async function statements(graph, nodePath, nodeLabel) {
       const subStatements = await statements(value, destPath, key);
       result = result.concat(subStatements);
     } else {
-      result.push(`  "${destPath}" [label="${key}"];`);
+      let label = value?.toString?.();
+      if (label) {
+        if (label.length > 20) {
+          label = label.slice(0, 20) + "…";
+        }
+        result.push(`  "${destPath}" [label="${label}"; shape=box];`);
+      }
     }
   }
   return result;
