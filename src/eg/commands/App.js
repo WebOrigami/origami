87716b41fb62992ce2c9c67d104a4455e8ab2596
Jan Miksovsky <jan@miksovsky.com>
2021-06-23 16:11:11 -0700

    app, file, and files command can resolve a relative path. app and files can also omit the relative path to work on the active directory.

--- src/eg/commands/App.js
+++ src/eg/commands/App.js
@@ -2,7 +2,7 @@ import path from "path";
 import process from "process";
 import ExplorableApp from "../../app/ExplorableApp.js";
 
-export default function app(relativePath) {
+export default function app(relativePath = "") {
   const resolvedPath = path.resolve(process.cwd(), relativePath);
   return new ExplorableApp(resolvedPath);
 }
--- src/eg/commands/file.js
+++ src/eg/commands/file.js
@@ -1,7 +1,9 @@
 import * as fs from "fs/promises";
+import path from "path";
 
-export default async function file(filePath) {
-  const data = await fs.readFile(filePath);
+export default async function file(relativePath) {
+  const resolvedPath = path.resolve(process.cwd(), relativePath);
+  const data = await fs.readFile(resolvedPath);
   return String(data);
 }
 
