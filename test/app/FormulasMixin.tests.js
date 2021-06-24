a0a2a5e717efe6e1c1e7c968afd49c2ce7b715cb
Jan Miksovsky <jan@miksovsky.com>
2021-06-24 11:05:12 -0700

    Test function invocation

--- test/app/FormulasMixin.tests.js
+++ test/app/FormulasMixin.tests.js
@@ -22,7 +22,7 @@ graph.scope = new Compose(
 );
 
 describe("FormulasMixin", () => {
-  it.only("keys include both real and virtual keys", async () => {
+  it("keys include both real and virtual keys", async () => {
     assert.deepEqual(await ExplorableGraph.keys(graph), [
       "foo.txt",
       "sample.txt",
@@ -42,10 +42,10 @@ describe("FormulasMixin", () => {
     assert.equal(await graph.get("sampleJson", "a"), "Hello, a.");
   });
 
-  // it("can export a function", async () => {
-  //   const fn = await graph.get("sample.txt");
-  //   assert.equal(fn(), "Hello, world.");
-  // });
+  it("can generate a value by calling a function exported by a module", async () => {
+    const fn = await graph.get("sample.txt");
+    assert.equal(fn(), "Hello, world.");
+  });
 
   it("can produce a value using a function", async () => {
     const value = await graph.get("value");
