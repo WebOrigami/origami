2356f9883ddd4cf93dcd4df8f1d856389677eee9
Jan Miksovsky <jan@miksovsky.com>
2021-10-01 16:21:07 -0700

    Remove commands which are old or have unclear value

--- src/eg/commands/Site.js
+++
@@ -1,7 +0,0 @@
-import ExplorableSite from "../../core/ExplorableSite.js";
-
-export default async function site(url) {
-  return new ExplorableSite(url);
-}
-
-site.usage = `site(url)\tThe explorable site at url`;
--- src/eg/commands/resolvePath.js
+++
@@ -1,8 +0,0 @@
-import path from "path";
-import process from "process";
-
-export default async function resolvePath(relativePath) {
-  return path.resolve(process.cwd(), relativePath);
-}
-
-resolvePath.usage = `resolvePath(path)\tResolve the path relative to the current directory`;
