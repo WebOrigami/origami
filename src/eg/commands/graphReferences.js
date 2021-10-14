c0c3b1662dc7b12b4ee07ac8f26d997e4734ea43
Jan Miksovsky <jan@miksovsky.com>
2021-10-13 18:03:03 -0700

    graphReferences can cast its argument to a graph

--- src/eg/commands/graphReferences.js
+++ src/eg/commands/graphReferences.js
@@ -2,7 +2,11 @@ import MetaGraph from "../../common/MetaGraph.js";
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 
 export default async function graphReferences(links) {
-  const meta = new MetaGraph(links, this);
+  const graph = ExplorableGraph.from(links);
+  const meta = new MetaGraph(graph, this.graph);
+  // TODO: Once the server can process explorable values with .json keys into
+  // JSON, can just return the meta graph instead of having to put it in JSON
+  // ourselves.
   const plain = await ExplorableGraph.plain(meta);
   return plain;
 }
