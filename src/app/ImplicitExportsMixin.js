c985dc29133c7a55c5253544cd9b6ba6f6686c5d
Jan Miksovsky <jan@miksovsky.com>
2021-10-14 09:20:24 -0700

    Don't expose implicit foo.js -> foo keys in iterator. Don't generate implicit keys if the original key is a formula containing "=".

--- src/app/ImplicitExportsMixin.js
+++ src/app/ImplicitExportsMixin.js
@@ -1,16 +1,5 @@
-import path from "path";
-
-export default function CommandsModulesMixin(Base) {
-  return class CommandModules extends Base {
-    async *[Symbol.asyncIterator]() {
-      for await (const key of super[Symbol.asyncIterator]()) {
-        yield key;
-        if (key.endsWith(".js")) {
-          yield path.basename(key, ".js");
-        }
-      }
-    }
-
+export default function ImplicitExportsMixin(Base) {
+  return class ImplicitExports extends Base {
     async get(...keys) {
       const value = await super.get(...keys);
       if (value !== undefined) {
@@ -19,6 +8,12 @@ export default function CommandsModulesMixin(Base) {
 
       // See if we have a JS module for the requested key.
       const lastKey = keys.pop();
+
+      // Don't define an value if the key is a formula.
+      if (lastKey.includes("=")) {
+        return undefined;
+      }
+
       const commandKey = `${lastKey}.js`;
       keys.push(commandKey);
       return await super.get(...keys);
