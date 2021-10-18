a8787494460f93c1e858f7c3c40fb76c95ce6c3f
Jan Miksovsky <jan@miksovsky.com>
2021-10-17 21:36:20 -0700

    map() command can apply multiple map functions

--- src/eg/commands/map.js
+++ src/eg/commands/map.js
@@ -1,7 +1,11 @@
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 
-export default async function map(graph, mapFn) {
-  return ExplorableGraph.map(graph, mapFn);
+export default async function map(graph, ...maps) {
+  let result = graph;
+  for (const map of maps) {
+    result = ExplorableGraph.map(result, map);
+  }
+  return result;
 }
 
 map.usage = `map(graph, fn)\tMap the values in a graph using a mapping function`;
