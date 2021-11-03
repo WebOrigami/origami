f7b0ef1a2aba6e0e299d65a56fc8563c60e948b1
Jan Miksovsky <jan@miksovsky.com>
2021-11-02 17:12:53 -0700

    Add syntax highlighting to markdown

--- src/eg/commands/mdHtml.js
+++ src/eg/commands/mdHtml.js
@@ -1,4 +1,13 @@
+import highlight from "highlight.js";
 import marked from "marked";
+
+marked.setOptions({
+  gfm: true, // Use GitHub-flavored markdown.
+  highlight: (code, lang) => {
+    const language = highlight.getLanguage(lang) ? lang : "plaintext";
+    return highlight.highlight(code, { language }).value;
+  },
+});
 
 export default async function mdHtml(markdown) {
   return marked(String(markdown));
