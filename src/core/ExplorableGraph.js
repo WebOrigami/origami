7121c3f2e3c2db95c2310ac474a7c2036b09acb3
Jan Miksovsky <jan@miksovsky.com>
2021-11-05 11:25:59 -0700

    Add Array to the list of types supported by canCastToExplorable

--- src/core/ExplorableGraph.js
+++ src/core/ExplorableGraph.js
@@ -14,7 +14,8 @@ export default class ExplorableGraph {
     return (
       this.isExplorable(obj) ||
       typeof obj === "string" ||
-      typeof obj === "function" ||
+      obj instanceof Function ||
+      obj instanceof Array ||
       isPlainObject(obj)
     );
   }
