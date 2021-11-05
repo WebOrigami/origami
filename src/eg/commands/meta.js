b7951fbac18434c63d062a5b444a7160dfc83080
Jan Miksovsky <jan@miksovsky.com>
2021-11-05 14:23:43 -0700

    meta() command inherits scope from current graph

--- src/eg/commands/meta.js
+++ src/eg/commands/meta.js
@@ -1,13 +1,14 @@
 import MetaMixin from "../../app/MetaMixin.js";
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 import { applyMixinToObject } from "../../core/utilities.js";
-import config from "./config.js";
 
 export default async function meta(variant = this.graph, ...path) {
   const graph = ExplorableGraph.from(variant);
+
   const meta = applyMixinToObject(MetaMixin, graph);
-  meta.scope = await config();
   meta.context = this.graph;
+  meta.scope = this.graph.scope;
+
   const result = path.length > 0 ? await meta.get(...path) : meta;
   return result;
 }
