04a1e8a8c566cdf98e5a84a39d9c28fe222e55ac
Jan Miksovsky <jan@miksovsky.com>
2021-11-07 08:23:43 -0800

    shell() command

---
+++ src/eg/commands/shell.js
@@ -0,0 +1,15 @@
+import { exec as callbackExec } from "child_process";
+import util from "util";
+const exec = util.promisify(callbackExec);
+
+export default async function shell(command) {
+  try {
+    const { stdout } = await exec(command);
+    return stdout;
+  } catch (err) {
+    console.error(err);
+    return undefined;
+  }
+}
+
+shell.usage = `shell(command)\tExecutes the shell command and returns the output`;
