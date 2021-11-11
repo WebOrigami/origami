c6160d3f4f3d4ccef3da64c5622e3cc0e6d11b5a
Jan Miksovsky <jan@miksovsky.com>
2021-11-10 17:32:47 -0800

    defaultIndexHtml combines formulas that generate a single key with the key they generate

--- src/app/defaultIndexHtml.js
+++ src/app/defaultIndexHtml.js
@@ -3,16 +3,29 @@ import ExplorableGraph from "../core/ExplorableGraph.js";
 export default async function defaultIndexHtml() {
   // @ts-ignore
   const graph = ExplorableGraph.from(this);
+  const keys = await ExplorableGraph.keys(graph);
+  const filtered = filterKeys(keys);
   const links = [];
-  for await (const key of graph) {
-    // Skip keys that start with a "." (like .keys.json).
-    if (!String(key).startsWith(".")) {
-      const isFormula = key.toString().includes("=");
-      const linkClass = isFormula ? `class="formula"` : "";
-      const link = `<li ${linkClass}><a href="${key}">${key}</a></li>`;
-      links.push(link);
+  for (const key of filtered) {
+    let link;
+    const isFormula = key.toString().includes("=");
+    const isWildcard = key.startsWith("{");
+    if (isFormula) {
+      if (isWildcard) {
+        link = `<li class="formula wildcard"><a href="${key}">${key}</a></li>`;
+      } else {
+        const parts = key.split("=");
+        const lhs = parts[0].trim();
+        const rhs = parts[1].trim();
+        link = `<li><a href="${lhs}">${lhs}</a> <span class="formula rhs"><a href="${key}">= ${rhs}</a></span></li>`;
+      }
+    } else {
+      // Simple key.
+      link = `<li><a href="${key}">${key}</a></li>`;
     }
+    links.push(link);
   }
+
   const parts = graph.path?.split("/");
   const heading = parts?.[parts.length - 1] ?? "Index";
   const list = `
@@ -36,6 +49,18 @@ export default async function defaultIndexHtml() {
           a:hover {
             text-decoration: revert;
           }
+
+          .formula {
+            color: #888;
+            font-family: monospace;
+          }
+          .formula a {
+            color: inherit;
+          }
+
+          .rhs {
+            margin-left: 1em;
+          }
         </style>
       </head>
       <body>
@@ -43,4 +68,25 @@ export default async function defaultIndexHtml() {
       </body>
     </html>`;
   return html.trim();
+}
+
+function filterKeys(keys) {
+  const filtered = [];
+  let previous = null;
+  for (const key of keys) {
+    if (key.startsWith(".")) {
+      // Skip "private" files.
+      continue;
+    }
+    if (previous && key.includes("=")) {
+      const [lhs, rhs] = previous.split("=");
+      if (lhs.trim() === previous) {
+        // Formula for the previous key replaces it.
+        filtered.pop();
+      }
+    }
+    filtered.push(key);
+    previous = key;
+  }
+  return filtered;
 }
