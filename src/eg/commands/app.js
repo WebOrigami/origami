7f0e23fec47f4752d29707d9837eac50896a8136
Jan Miksovsky <jan@miksovsky.com>
2021-11-10 21:41:36 -0800

    app() command takes its first argument as a graph to wrap

--- src/eg/commands/app.js
+++ src/eg/commands/app.js
@@ -1,20 +1,16 @@
-import path from "path";
-import process from "process";
 import DefaultPages from "../../app/DefaultPages.js";
 // import DefaultPagesMixin from "../../app/DefaultPagesMixin.js";
 import MetaMixin from "../../app/MetaMixin.js";
+import ExplorableGraph from "../../core/ExplorableGraph.js";
 import { applyMixinToObject } from "../../core/utilities.js";
-import ExplorableFiles from "../../node/ExplorableFiles.js";
-import config from "./config.js";
 
 // Start an ExplorableApp
-export default async function app(...keys) {
-  const appPath = path.resolve(process.cwd(), ...keys);
-  let graph = new ExplorableFiles(appPath);
+export default async function app(variant = this.graph, ...keys) {
+  const graph = ExplorableGraph.from(variant);
   const meta = applyMixinToObject(MetaMixin, graph);
   const result = new DefaultPages(meta);
-  result.scope = await config(appPath);
-  return result;
+  result.scope = this.scope;
+  return keys.length > 0 ? await result.get(...keys) : result;
 }
 
 app.usage = `app()\tAn explorable application graph for the current directory`;
--- src/eg/commands/serve.js
+++ src/eg/commands/serve.js
@@ -1,15 +1,16 @@
 import http from "http";
 import process from "process";
+import ExplorableGraph from "../../core/ExplorableGraph.js";
 import { requestListener } from "../../server/server.js";
 import app from "./app.js";
 import watch from "./watch.js";
 
 const defaultPort = process.env.PORT || 5000;
 
-export default async function serve(graph, port = defaultPort) {
-  if (graph === undefined) {
-    graph = await watch(await app());
-  }
+export default async function serve(variant, port = defaultPort) {
+  const graph = variant
+    ? ExplorableGraph.from(variant)
+    : await watch.call(this, await app.call(this));
   http.createServer(requestListener(graph)).listen(port);
   console.log(`Server running at http://localhost:${port}`);
 }
