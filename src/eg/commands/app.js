ecfe50471909190c67f79f4b27353973f5b38f74
Jan Miksovsky <jan@miksovsky.com>
2021-11-14 14:25:27 -0800

    Default app() config provided if app() is invoked directly

--- src/eg/commands/app.js
+++ src/eg/commands/app.js
@@ -3,13 +3,14 @@ import DefaultPages from "../../app/DefaultPages.js";
 import MetaMixin from "../../app/MetaMixin.js";
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 import { applyMixinToObject } from "../../core/utilities.js";
+import config from "./config.js";
 
 // Start an ExplorableApp
 export default async function app(variant = this.graph, ...keys) {
   const graph = ExplorableGraph.from(variant);
   const meta = applyMixinToObject(MetaMixin, graph);
   const result = new DefaultPages(meta);
-  result.scope = this.scope;
+  result.scope = this?.scope ?? (await config());
   return keys.length > 0 ? await result.get(...keys) : result;
 }
 
