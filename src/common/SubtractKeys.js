59c8c2d595764d3d3832ecb03a4ca82be58be03e
Jan Miksovsky <jan@miksovsky.com>
2021-11-15 22:05:38 -0800

    Try out isKeyExplorable in practice to avoid a potentially more expensive get operation

--- src/common/SubtractKeys.js
+++ src/common/SubtractKeys.js
@@ -1,29 +1,39 @@
 import ExplorableGraph from "../core/ExplorableGraph.js";
 
-const originalKey = Symbol("originalKey");
-const removeKey = Symbol("removeKey");
+const originalGraph = Symbol("originalKey");
+const removeGraph = Symbol("removeKey");
 
 export default class SubtractKeys {
   constructor(original, remove) {
-    this[originalKey] = ExplorableGraph.from(original);
-    this[removeKey] = ExplorableGraph.from(remove);
+    this[originalGraph] = ExplorableGraph.from(original);
+    this[removeGraph] = ExplorableGraph.from(remove);
   }
 
   async *[Symbol.asyncIterator]() {
-    for await (const key of this[originalKey]) {
-      const removeResult = await this[removeKey].get(key);
-      if (
-        removeResult === undefined ||
-        ExplorableGraph.isExplorable(removeResult)
-      ) {
+    const removeKeys = await ExplorableGraph.keys(this[removeGraph]);
+    for await (const key of this[originalGraph]) {
+      if (!removeKeys.includes(key)) {
+        // Remove graph doesn't have anything to say about this key, so we'll
+        // include it.
         yield key;
+      } else {
+        // Remove graph has this key, but if the key's explorable, we want to
+        // yield so that the traversals can consider this portion of the remove
+        // graph.
+        const isKeyExplorable = await ExplorableGraph.isKeyExplorable(
+          this[removeGraph],
+          key
+        );
+        if (isKeyExplorable) {
+          yield key;
+        }
       }
     }
   }
 
   async get(...keys) {
-    let originalValue = await this[originalKey].get(...keys);
-    const removeValue = await this[removeKey].get(...keys);
+    let originalValue = await this[originalGraph].get(...keys);
+    const removeValue = await this[removeGraph].get(...keys);
     if (ExplorableGraph.isExplorable(originalValue)) {
       if (ExplorableGraph.isExplorable(removeValue)) {
         originalValue = new SubtractKeys(originalValue, removeValue);
