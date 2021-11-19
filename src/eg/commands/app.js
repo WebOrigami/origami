85df8e1bfdab04a60b982b7f033d80afc6d107e0
Jan Miksovsky <jan@miksovsky.com>
2021-11-19 11:28:45 -0800

    app() constructs new graph if none is supplied rather than extending the default graph

--- src/eg/commands/app.js
+++ src/eg/commands/app.js
@@ -1,14 +1,22 @@
 // import defaultPages from "../../app/defaultPages.js";
+import path from "path";
 import MetaMixin from "../../app/MetaMixin.js";
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 import { applyMixinToObject } from "../../core/utilities.js";
+import ExplorableFiles from "../../node/ExplorableFiles.js";
 import config from "./config.js";
 
 // Start an ExplorableApp
-export default async function app(variant = this.graph, ...keys) {
-  const graph = ExplorableGraph.from(variant);
-  const meta = applyMixinToObject(MetaMixin, graph);
-  meta.scope = this?.scope ?? (await config());
+export default async function app(variant, ...keys) {
+  let meta;
+  if (variant) {
+    const graph = ExplorableGraph.from(variant);
+    meta = applyMixinToObject(MetaMixin, graph);
+  } else {
+    const dirname = path.resolve(process.cwd(), ...keys);
+    meta = new (MetaMixin(ExplorableFiles))(dirname);
+    meta.scope = await config(dirname);
+  }
   // meta.inheritedFallbacks = defaultPages;
   return keys.length > 0 ? await ExplorableGraph.traverse(meta, ...keys) : meta;
 }
