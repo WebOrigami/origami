535e09682d129d89e7eb20fb7849b715471f3e6a
Jan Miksovsky <jan@miksovsky.com>
2021-12-03 20:48:57 -0800

    Add more tests to browser suite

--- test/browserTests.js
+++ test/browserTests.js
@@ -1,9 +1,19 @@
 // Should build this list of imports with ExplorableGraph
+import "./app/AdditionsMixin.tests.js";
+import "./app/FormulasMixin.tests.js";
+// import "./app/GhostValuesMixin.tests.js";
+import "./app/InheritScopeMixin.tests.js";
+// import "./app/MetaMixin.tests.js";
 import "./common/Cache.tests.js";
 import "./common/Compose.tests.js";
+import "./common/DefaultValues.tests.js";
+import "./common/SubtractKeys.tests.js";
+import "./core/ExplorableArray.tests.js";
 import "./core/ExplorableFunction.tests.js";
 import "./core/ExplorableGraph.tests.js";
 import "./core/ExplorableObject.tests.js";
+import "./core/MapGraph.tests.js";
+import "./core/utilities.tests.js";
 // import "./core/ExplorableSite.tests.js";
 import "./eg/execute.tests.js";
 import "./eg/parse.tests.js";
