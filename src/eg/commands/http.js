1bff56f642ee264515243a4b0a5e1ffbcfdbb6fe
Jan Miksovsky <jan@miksovsky.com>
2022-01-05 15:48:50 -0800

    Add http() command

---
+++ src/eg/commands/http.js
@@ -0,0 +1,9 @@
+import ExplorableGraph from "../../core/ExplorableGraph.js";
+import ExplorableSite from "../../core/ExplorableSite.js";
+
+export default async function http(domain, ...keys) {
+  const site = new ExplorableSite(`http://${domain}`);
+  return await ExplorableGraph.traverse(site, ...keys);
+}
+
+http.usage = `http(domain, ...keys)\tA web resource`;
