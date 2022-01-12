2c93c538a628f67d58a95a43be7e18d8e54b2261
Jan Miksovsky <jan@miksovsky.com>
2022-01-12 11:57:41 -0800

    parse() returned undefined if text is undefined

--- src/eg/commands/parse.js
+++ src/eg/commands/parse.js
@@ -1,7 +1,7 @@
 import * as utilities from "../../core/utilities.js";
 
 export default async function parse(text) {
-  return utilities.parse(text);
+  return text ? utilities.parse(text) : undefined;
 }
 
 parse.usage = `parse(text)\tParse text as JSON or YAML into an object`;
