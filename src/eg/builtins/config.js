df2bffd62f0a5e8b6c034d744d76db383fac42bf
Jan Miksovsky <jan@miksovsky.com>
2022-01-14 10:53:06 -0800

    Move defaultModuleExport out of builtins, as at this point it's unclear whether eg users need direct access to it

--- src/eg/builtins/config.js
+++ src/eg/builtins/config.js
@@ -1,7 +1,7 @@
 import ExplorableGraph from "../../core/ExplorableGraph.js";
 import ParentFiles from "../../node/ParentFiles.js";
 import builtins from "../builtins.js";
-import defaultModuleExport from "./defaultModuleExport.js";
+import defaultModuleExport from "../defaultModuleExport.js";
 
 const configFileName = "eg.config.js";
 
