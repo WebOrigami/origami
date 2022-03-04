fed69d9835f0d5fc53c7c973d3449395ddec91c6
Jan Miksovsky <jan@miksovsky.com>
2022-03-04 10:33:37 -0800

    Unit tests for templates

---
+++ test/app/PikaTemplate.tests.js
@@ -0,0 +1,75 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import { ExplorableFiles } from "../../exports.js";
+import PikaTemplate from "../../src/app/PikaTemplate.js";
+import ExplorableGraph from "../../src/core/ExplorableGraph.js";
+import ExplorableObject from "../../src/core/ExplorableObject.js";
+import map from "../../src/eg/builtins/map.js";
+import shallowMap from "../../src/eg/builtins/shallowMap.js";
+import assert from "../assert.js";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
+const templatesDirectory = path.join(dirname, "fixtures/templates");
+const templateFiles = new ExplorableFiles(templatesDirectory);
+
+describe("PikaTemplate", () => {
+  it("can make substitutions from input and context", async () => {
+    const template = new PikaTemplate("{{greeting}}, {{name}}.");
+    const input = { name: "world" };
+    const context = new ExplorableObject({ greeting: "Hello" });
+    const result = await template.apply(input, context);
+    assert.equal(result, "Hello, world.");
+  });
+
+  it("can inline files", async () => {
+    const template = new PikaTemplate(
+      `This template inlines a file.
+{{ plain.txt }}`
+    );
+    const result = await template.apply(null, templateFiles);
+    assert.equal(
+      result,
+      `This template inlines a file.
+Hello, world.
+`
+    );
+  });
+
+  it("can map data to a nested template", async () => {
+    const template = new PikaTemplate(
+      "Greetings:\n{{shallowMap(people, pika`{{greeting}}, {{name}}.\n`)}}"
+    );
+    const graph = ExplorableGraph.from({
+      greeting: "Hello",
+      people: [{ name: "Alice" }, { name: "Bob" }, { name: "Carol" }],
+      shallowMap,
+    });
+    const result = await template.apply(null, graph);
+    assert.equal(
+      result,
+      `Greetings:
+Hello, Alice.
+Hello, Bob.
+Hello, Carol.
+`
+    );
+  });
+
+  it("gives template access to @key and @value", async () => {
+    const template = new PikaTemplate(
+      "{{ map(array, pika`{{ @key }}: {{ @value }}\n`) }}"
+    );
+    const graph = ExplorableGraph.from({
+      array: ["a", "b", "c"],
+      map,
+    });
+    const result = await template.apply(null, graph);
+    assert.equal(
+      result,
+      `0: a
+1: b
+2: c
+`
+    );
+  });
+});
--- test/eg/pkt.tests.js
+++ test/eg/pkt.tests.js
@@ -1,9 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
-import ExplorableGraph from "../../src/core/ExplorableGraph.js";
-import map from "../../src/eg/builtins/map.js";
 import pkt from "../../src/eg/builtins/pkt.js";
-import shallowMap from "../../src/eg/builtins/shallowMap.js";
 import ExplorableFiles from "../../src/node/ExplorableFiles.js";
 import ImplicitModulesTransform from "../../src/node/ImplicitModulesTransform.js";
 import assert from "../assert.js";
@@ -16,7 +13,7 @@ const fixturesGraph = new (ImplicitModulesTransform(ExplorableFiles))(
 
 describe("pkt (pika template)", () => {
   it("substitutes values from the supplied graph", async () => {
-    const template = await fixturesGraph.get("template.pkt");
+    const template = await fixturesGraph.get("inline.pkt");
     const result = await pkt.call(fixturesGraph, template);
     const normalized = result.toString().replace(/\r\n/g, "\n");
     assert.equal(
@@ -26,41 +23,6 @@ describe("pkt (pika template)", () => {
 Hello, world.
 
 Hello, Alice.
-`
-    );
-  });
-
-  it("can map data to a nested template", async () => {
-    const template =
-      "Greetings:\n{{shallowMap(people, pika`{{greeting}}, {{name}}.\n`)}}";
-    const graph = ExplorableGraph.from({
-      greeting: "Hello",
-      people: [{ name: "Alice" }, { name: "Bob" }, { name: "Carol" }],
-      shallowMap,
-    });
-    const result = await pkt.call(graph, template);
-    assert.equal(
-      result,
-      `Greetings:
-Hello, Alice.
-Hello, Bob.
-Hello, Carol.
-`
-    );
-  });
-
-  it("template has access to @key and @value", async () => {
-    const template = "{{ map(array, pika`{{ @key }}: {{ @value }}\n`) }}";
-    const graph = ExplorableGraph.from({
-      array: ["a", "b", "c"],
-      map,
-    });
-    const result = await pkt.call(graph, template);
-    assert.equal(
-      result,
-      `0: a
-1: b
-2: c
 `
     );
   });
