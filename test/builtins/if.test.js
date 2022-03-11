83c1d863be4f60824e57f289bce39b289a28e6a7
Jan Miksovsky <jan@miksovsky.com>
2022-03-11 15:33:54 -0800

    Fix if() test

--- test/builtins/if.test.js
+++ test/builtins/if.test.js
@@ -2,9 +2,9 @@ import ifBuiltin from "../../src/builtins/if.js";
 import assert from "../assert.js";
 
 describe("if", () => {
-  it("conditionally returns a value", () => {
-    assert.equal(ifBuiltin(true, "true"), "true");
-    assert.equal(ifBuiltin(false, "true"), undefined);
-    assert.equal(ifBuiltin(false, "true", "false"), "false");
+  it("conditionally returns a value", async () => {
+    assert.equal(await ifBuiltin(true, "true"), "true");
+    assert.equal(await ifBuiltin(false, "true"), undefined);
+    assert.equal(await ifBuiltin(false, "true", "false"), "false");
   });
 });
