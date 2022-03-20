e437233e51e9e5d8b1cf2b5ef27561cf96491b68
Jan Miksovsky <jan@miksovsky.com>
2022-03-20 15:14:43 -0700

    unless() command for "if not" situations in templates

---
+++ src/builtins/unless.js
@@ -0,0 +1,18 @@
+import ExplorableGraph from "../core/ExplorableGraph.js";
+
+export default async function unless(value, falseResult) {
+  let condition = await value;
+  if (ExplorableGraph.isExplorable(condition)) {
+    const keys = await ExplorableGraph.keys(condition);
+    condition = keys.length > 0;
+  }
+
+  let result = condition ? undefined : falseResult;
+  if (typeof result === "function") {
+    result = await result();
+  }
+  return result;
+}
+
+unless.usage = `unless <value>, <result>]\tReturns the result if value is falsy`;
+unless.documentation = "https://explorablegraph.org/cli/builtins.html#unless";
