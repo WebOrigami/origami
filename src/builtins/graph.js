58f3064021f5edaa43b8cda921378834c03e3119
Jan Miksovsky <jan@miksovsky.com>
2022-03-22 10:10:19 -0700

    graph() defaults to current graph if no parameter is supplied

--- src/builtins/graph.js
+++ src/builtins/graph.js
@@ -1,7 +1,13 @@
 import ExplorableGraph from "../core/ExplorableGraph.js";
 
+/**
+ * Cast the indicated variant to a graph.
+ *
+ * @this {Explorable}
+ * @param {GraphVariant} [variant]
+ */
 export default async function graph(variant) {
-  return ExplorableGraph.from(variant);
+  return variant ? ExplorableGraph.from(variant) : this;
 }
 
 graph.usage = `graph <variant>\tConvert JSON, YAML, function, or plain object to a graph`;
