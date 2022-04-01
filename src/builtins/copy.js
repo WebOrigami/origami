26ea14eb6c8091dfb26b3f1f3fa7ede48473efa8
Jan Miksovsky <jan@miksovsky.com>
2022-04-01 16:23:51 -0700

    Leave in some performance measuring code but keep it commented out

--- src/builtins/copy.js
+++ src/builtins/copy.js
@@ -1,9 +1,12 @@
 import ExplorableGraph from "../core/ExplorableGraph.js";
 
 export default async function copy(source, target) {
+  // const start = performance.now();
   const sourceGraph = ExplorableGraph.from(source);
   /** @type {any} */ const targetGraph = ExplorableGraph.from(target);
   await targetGraph.set(sourceGraph);
+  // const end = performance.now();
+  // console.log(`copy time in ms: ${end - start}`);
 }
 
 copy.usage = `copy <source>, <target>\tCopies the source graph to the target`;
