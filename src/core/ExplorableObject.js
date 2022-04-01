6c0f5246cae52522f65d6bc47b5cbefab5a514e4
Jan Miksovsky <jan@miksovsky.com>
2022-04-01 16:08:31 -0700

    Parallelize ExplorableObject set()

--- src/core/ExplorableObject.js
+++ src/core/ExplorableObject.js
@@ -66,20 +66,32 @@ export default class ExplorableObject {
   }
 }
 
+// Apply all updates from the source to the target.
 async function applyUpdates(source, target) {
+  // Fire off requests to update all keys, then wait for all of them to finish.
+  const promises = [];
   for await (const key of source) {
-    const sourceValue = await source.get(key);
-    if (ExplorableGraph.isExplorable(sourceValue)) {
-      const targetValue = await target.get(key);
-      if (ExplorableGraph.isExplorable(targetValue)) {
-        await applyUpdates(sourceValue, targetValue);
-        continue;
-      }
-    }
-    if (sourceValue === undefined) {
-      delete target.object[key];
-    } else {
-      target.object[key] = sourceValue;
+    const updateKeyPromise = applyUpdateForKey(source, target, key);
+    promises.push(updateKeyPromise);
+  }
+  return Promise.all(promises);
+}
+
+// Copy the value for the given key from the source to the target.
+async function applyUpdateForKey(source, target, key) {
+  const sourceValue = await source.get(key);
+  if (sourceValue === undefined) {
+    // Undefined source value means delete the key from the target.
+    delete target.object[key];
+    return;
+  } else if (ExplorableGraph.isExplorable(sourceValue)) {
+    const targetValue = await target.get(key);
+    if (ExplorableGraph.isExplorable(targetValue)) {
+      // Both source and target are explorable; recurse.
+      await applyUpdates(sourceValue, targetValue);
+      return;
     }
   }
+  // Copy the value from the source to the target.
+  target.object[key] = sourceValue;
 }
