1f1a23d7f66818a1111f0f02736987f4e0d49c53
Jan Miksovsky <jan@miksovsky.com>
2022-04-13 16:43:50 +0200

    Change scope() to once again return the scope of an indicated graph

--- src/builtins/scope.js
+++ src/builtins/scope.js
@@ -1,14 +1,20 @@
 import ExplorableGraph from "../core/ExplorableGraph.js";
 
 /**
- * Returns the scope of the current graph.
+ * Returns the scope of the indicated graph.
  *
  * @this {Explorable}
- * @param {any[]} keys
+ * @param {GraphVariant} variant
  */
-export default async function scope(...keys) {
-  return ExplorableGraph.traverse(this, ...keys);
+export default async function scope(variant) {
+  variant = variant ?? (await this?.get("@defaultGraph"));
+  if (variant === undefined) {
+    return undefined;
+  }
+  const graph = ExplorableGraph.from(variant);
+  const scope = /** @type {any} */ (graph).scope;
+  return scope;
 }
 
-scope.usage = `scope [...keys]\tReturns the current scope`;
+scope.usage = `scope <graph>]\tReturns the scope of the graph`;
 scope.documentation = "https://explorablegraph.org/cli/builtins.html#scope";
