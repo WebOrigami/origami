06caafc665c87b7ea207fd2070329860ec16a8c4
Jan Miksovsky <jan@miksovsky.com>
2022-05-09 13:32:34 -0700

    Turn of watch() in serve() until we can verify its reliability

--- src/builtins/serve.js
+++ src/builtins/serve.js
@@ -3,7 +3,7 @@ import process from "process";
 import ExplorableGraph from "../core/ExplorableGraph.js";
 import { requestListener } from "../server/server.js";
 import virtual from "./virtual.js";
-import watch from "./watch.js";
+// import watch from "./watch.js";
 
 const defaultPort = process.env.PORT || 5000;
 
@@ -11,7 +11,8 @@ export default async function serve(variant, port = defaultPort) {
   const graph = variant
     ? ExplorableGraph.from(variant)
     : // @ts-ignore
-      await watch.call(this, await virtual.call(this));
+      // await watch.call(this, await virtual.call(this));
+      await await virtual.call(this);
   http.createServer(requestListener(graph)).listen(port, undefined, () => {
     console.log(
       `Server running at http://localhost:${port}. Press Ctrl+C to stop.`
