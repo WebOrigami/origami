ca0d94d4ea4829060f4fe9856ee76d021826fa0e
Jan Miksovsky <jan@miksovsky.com>
2022-08-24 14:01:26 -0700

    make() takes advantage of meta()

--- src/builtins/make.js
+++ src/builtins/make.js
@@ -1,10 +1,8 @@
 import SubtractKeys from "../common/SubtractKeys.js";
-import ExplorableGraph from "../core/ExplorableGraph.js";
 import MapValuesGraph from "../core/MapValuesGraph.js";
-import { transformObject } from "../core/utilities.js";
-import MetaTransform from "../framework/MetaTransform.js";
 import clean from "./clean.js";
 import copy from "./copy.js";
+import meta from "./meta.js";
 import reals from "./reals.js";
 import yaml from "./yaml.js";
 
@@ -15,31 +13,22 @@ import yaml from "./yaml.js";
  * @param {GraphVariant} variant
  */
 export default async function make(variant) {
-  variant = variant ?? (await this?.get("@defaultGraph"));
-  if (variant === undefined) {
-    return;
-  }
-  let graph = ExplorableGraph.from(variant);
-  if (!("formulas" in graph)) {
-    graph = transformObject(MetaTransform, graph);
-  }
+  const graph = await meta.call(this, variant);
   await clean(graph);
-  const plainGraphStart = await ExplorableGraph.plain(graph);
+  // const plainGraphStart = await ExplorableGraph.plain(graph);
   const real = await reals(graph);
-  const plainReal = await ExplorableGraph.plain(real);
-  // const filter = new MapValuesGraph(real, (value) => false, { deep: true });
-  // const build = new FilterGraph(graph, filter);
+  // const plainReal = await ExplorableGraph.plain(real);
   const build = new SubtractKeys(graph, real);
-  const plainBuild = await ExplorableGraph.plain(build);
+  // const plainBuild = await ExplorableGraph.plain(build);
   const undefineds = new MapValuesGraph(build, (value) => undefined, {
     deep: true,
   });
-  const plainUndefineds = await ExplorableGraph.plain(undefineds);
+  // const plainUndefineds = await ExplorableGraph.plain(undefineds);
   const cleanYaml = await yaml(undefineds);
   await graph.set(".ori.clean.yaml", cleanYaml);
   await copy(undefineds, graph);
   await copy(build, graph);
-  const plainGraphEnd = await ExplorableGraph.plain(graph);
+  // const plainGraphEnd = await ExplorableGraph.plain(graph);
 }
 
 make.usage = `make\tMake real versions of any virtual values [experimental]`;
