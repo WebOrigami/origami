c55ce8efe222edf2470c41ab9198904a7cc4a557
Jan Miksovsky <jan@miksovsky.com>
2022-09-12 12:08:42 -0700

    Quick copy() progress output

--- src/builtins/copy.js
+++ src/builtins/copy.js
@@ -1,12 +1,54 @@
+import process, { stdout } from "node:process";
 import ExplorableGraph from "../core/ExplorableGraph.js";
+import { transformObject } from "../core/utilities.js";
 
 export default async function copy(source, target) {
   // const start = performance.now();
   const sourceGraph = ExplorableGraph.from(source);
-  /** @type {any} */ const targetGraph = ExplorableGraph.from(target);
+  /** @type {any} */ let targetGraph = ExplorableGraph.from(target);
+
+  if (stdout.isTTY) {
+    targetGraph = transformObject(ProgressTransform, targetGraph);
+    copyRoot = targetGraph;
+    countFiles = 0;
+    countCopied = 0;
+  }
+
   await targetGraph.set(sourceGraph);
+
+  if (stdout.isTTY) {
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    copyRoot = null;
+    countFiles = null;
+    countCopied = null;
+  }
+
   // const end = performance.now();
   // console.log(`copy time in ms: ${end - start}`);
+}
+
+let countFiles;
+let countCopied;
+let copyRoot;
+
+function ProgressTransform(Base) {
+  return class Progress extends Base {
+    async set(...args) {
+      countFiles++;
+      copyRoot.showProgress();
+      const result = await super.set(...args);
+      countCopied++;
+      copyRoot.showProgress();
+      return result;
+    }
+
+    showProgress() {
+      process.stdout.clearLine(0);
+      process.stdout.cursorTo(0);
+      process.stdout.write(`Copied ${countCopied} of ${countFiles}`);
+    }
+  };
 }
 
 copy.usage = `copy <source>, <target>\tCopies the source graph to the target`;
