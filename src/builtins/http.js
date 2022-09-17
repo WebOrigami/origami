f7a3e0a5d631d17811f31ce1159d93815b243b0e
Jan Miksovsky <jan@miksovsky.com>
2022-09-17 10:34:20 -0700

    http/https can return a regular web resource if the path does not end in a slash

--- src/builtins/http.js
+++ src/builtins/http.js
@@ -1,10 +1,21 @@
+import fetch from "node-fetch";
 import ExplorableGraph from "../core/ExplorableGraph.js";
 import SiteGraph from "../core/SiteGraph.js";
 
 export default async function http(domain, ...keys) {
-  const site = new SiteGraph(`http://${domain}`);
-  return ExplorableGraph.traverse(site, ...keys);
+  if (keys.length > 0 && keys[keys.length - 1] === undefined) {
+    const site = new SiteGraph(`http://${domain}`);
+    return ExplorableGraph.traverse(site, ...keys);
+  } else {
+    const url = ["http:/", domain, ...keys].join("/");
+    const response = await fetch(url);
+    if (response.ok) {
+      const arrayBuffer = await response.arrayBuffer();
+      const buffer = Buffer.from(arrayBuffer);
+      return buffer;
+    }
+  }
 }
 
 http.usage = `http <domain>, <...keys>\tA web resource via HTTP`;
-http.documentation = "https://explorablegraph.org/cli/builtins.html#http";
+http.documentation = "http://explorablegraph.org/cli/builtins.html#http";
