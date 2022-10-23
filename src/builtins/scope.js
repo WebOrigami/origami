bf320f1d82de9e521165ed689a0cb34240f8190f
Jan Miksovsky <jan@miksovsky.com>
2022-10-22 17:17:37 -0700

    Allow scope() to be called with no parameters to get the current scope

--- src/builtins/scope.js
+++ src/builtins/scope.js
@@ -1,20 +1,21 @@
 import ExplorableGraph from "../core/ExplorableGraph.js";
 
 /**
- * Returns the scope of the indicated graph.
+ * Returns the scope of the indicated graph or the current scope.
  *
  * @this {Explorable}
- * @param {GraphVariant} variant
+ * @param {GraphVariant} [variant]
  */
 export default async function scope(variant) {
-  variant = variant ?? (await this?.get("@defaultGraph"));
-  if (variant === undefined) {
-    return undefined;
+  let scope;
+  if (variant) {
+    const graph = ExplorableGraph.from(variant);
+    scope = /** @type {any} */ (graph).scope;
+  } else {
+    scope = this;
   }
-  const graph = ExplorableGraph.from(variant);
-  const scope = /** @type {any} */ (graph).scope;
   return scope;
 }
 
-scope.usage = `scope <graph>]\tReturns the scope of the graph`;
+scope.usage = `scope [<graph>]\tReturns the scope of the graph or the current scope`;
 scope.documentation = "https://graphorigami.org/cli/builtins.html#scope";
