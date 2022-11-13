3576d9b8a879482058f26ac892d300f9690579a0
Jan Miksovsky <jan@miksovsky.com>
2022-11-12 21:58:47 -0800

    image/format()

--- src/builtins/image.js
+++ src/builtins/image.js
@@ -1,6 +1,12 @@
 import sharp from "sharp";
 
 export default {
+  format(imageBuffer, format, options) {
+    return imageBuffer instanceof Buffer
+      ? sharp(imageBuffer).toFormat(format, options).toBuffer()
+      : undefined;
+  },
+
   resize(imageBuffer, options) {
     return imageBuffer instanceof Buffer
       ? sharp(imageBuffer).resize(options).toBuffer()
