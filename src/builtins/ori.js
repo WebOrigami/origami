6ef986fc5a0e22df49c702d9e1bdbb0d36d1d347
Jan Miksovsky <jan@miksovsky.com>
2022-11-21 15:59:17 -0800

    Remove `path` parameter from ori() builtin; it's too specific

--- src/builtins/ori.js
+++ src/builtins/ori.js
@@ -2,11 +2,7 @@
 
 import yaml from "../builtins/yaml.js";
 import builtins from "../cli/builtins.js";
-import ExplorableGraph from "../core/ExplorableGraph.js";
 import { incrementCount } from "../core/measure.js";
-import { transformObject } from "../core/utilities.js";
-import InheritScopeTransform from "../framework/InheritScopeTransform.js";
-import { getScope } from "../framework/scopeUtilities.js";
 import execute from "../language/execute.js";
 import * as parse from "../language/parse.js";
 
@@ -19,7 +15,7 @@ import * as parse from "../language/parse.js";
  * @param {string} [path]
  * @returns {Promise<string | String | Buffer | undefined>}
  */
-export default async function ori(expression, path) {
+export default async function ori(expression) {
   // In case expression is a Buffer, cast it to a string.
   expression = String(expression).trim();
 
@@ -33,25 +29,6 @@ export default async function ori(expression, path) {
   if (!code || parsed.rest !== "") {
     console.error(`ori: could not recognize expression: ${expression}`);
     return;
-  }
-
-  // If a path was provided, traverse that before evaluating the code.
-  //
-  // REVIEW: This path-traversing feature of ori exists to support asserts,
-  // which often need to traverse a graph before evaluating an assertion. That
-  // feels too specific to support in this otherwise general-purpose function.
-  // The use of slash-separated paths also feels too specific.
-  if (path) {
-    const keys = path.split("/");
-    const [first, ...rest] = keys;
-    let graph = await scope.get(first);
-    if (!graph) {
-      return undefined;
-    }
-    graph = transformObject(InheritScopeTransform, graph);
-    graph.parent = scope;
-    graph = await ExplorableGraph.traverse(graph, ...rest);
-    scope = getScope(graph);
   }
 
   // Execute
