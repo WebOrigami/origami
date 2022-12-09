fa11062f42a7a85f8c3c799ae9641c07fe42568f
Jan Miksovsky <jan@miksovsky.com>
2022-12-09 10:31:19 -0800

    Remove CachedValuesTransform from MetaTransform for now

--- src/framework/MetaTransform.js
+++ src/framework/MetaTransform.js
@@ -1,4 +1,3 @@
-import CachedValuesTransform from "../common/CachedValuesTransform.js";
 import FileLoadersTransform from "../common/FileLoadersTransform.js";
 import ImplicitModulesTransform from "../common/ImplicitModulesTransform.js";
 import FormulasTransform from "./FormulasTransform.js";
@@ -7,21 +6,11 @@ import KeysTransform from "./KeysTransform.js";
 import PathTransform from "./PathTransform.js";
 
 export default function MetaTransform(Base) {
-  return class Meta extends CachedValuesTransform(
-    PathTransform(
-      InheritScopeTransform(
-        FormulasTransform(
-          FileLoadersTransform(ImplicitModulesTransform(KeysTransform(Base)))
-        )
+  return class Meta extends PathTransform(
+    InheritScopeTransform(
+      FormulasTransform(
+        FileLoadersTransform(ImplicitModulesTransform(KeysTransform(Base)))
       )
     )
-  ) {
-    constructor(...args) {
-      super(...args);
-    }
-
-    isKeyCachable(key) {
-      return (key.endsWith?.(".meta") || key.endsWith?.(".vfiles")) ?? false;
-    }
-  };
+  ) {};
 }
