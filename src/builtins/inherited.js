7233369050d0abc755a23a684abeabb74357406b
Jan Miksovsky <jan@miksovsky.com>
2023-02-06 16:56:31 -0800

    Rename `…` command back to `inherited`

---
+++ src/builtins/inherited.js
@@ -0,0 +1,15 @@
+import * as ops from "../language/ops.js";
+
+/**
+ * Return the inherited value (if any) for the indicated key.
+ *
+ * @param {any} key
+ * @this {Explorable}
+ */
+export default async function inherited(key) {
+  return ops.inherited.call(this, key);
+}
+
+inherited.usage = `inherited <key>\tThe value of the key in the graph's inherited scope`;
+inherited.documentation =
+  "https://graphorigami.org/cli/builtins.html#inherited";
