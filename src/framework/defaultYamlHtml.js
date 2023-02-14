c5ba87f2e9284f68b5e4b68162e58bc02aeece56
Jan Miksovsky <jan@miksovsky.com>
2023-02-14 09:54:51 -0800

    Attach input graph to returned HTML

--- src/framework/defaultYamlHtml.js
+++ src/framework/defaultYamlHtml.js
@@ -1,3 +1,4 @@
+import StringWithGraph from "../common/StringWithGraph.js";
 import ExplorableGraph from "../core/ExplorableGraph.js";
 
 export default async function defaultYamlHtml() {
@@ -10,42 +11,41 @@ export default async function defaultYamlHtml() {
     keyRegex,
     (match, key) => `<a href="${key}">${match}</a>`
   );
-  const html = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <style>
-          li {
-            margin-bottom: 0.20em;
-          }
+  const html = `<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <style>
+      li {
+        margin-bottom: 0.20em;
+      }
 
-          a {
-            text-decoration: none;
-          }
-          a:hover {
-            text-decoration: revert;
-          }
+      a {
+        text-decoration: none;
+      }
+      a:hover {
+        text-decoration: revert;
+      }
 
-          .formula {
-            color: #888;
-            font-family: monospace;
-          }
-          .formula a {
-            color: inherit;
-          }
+      .formula {
+        color: #888;
+        font-family: monospace;
+      }
+      .formula a {
+        color: inherit;
+      }
 
-          .rhs {
-            margin-left: 1em;
-          }
-        </style>
-      </head>
-      <body>
-        <pre>${replaced}</pre>
-      </body>
-    </html>`;
-  return html.trim();
+      .rhs {
+        margin-left: 1em;
+      }
+    </style>
+  </head>
+  <body>
+    <pre>${replaced}</pre>
+  </body>
+</html>`;
+  return new StringWithGraph(html, graph);
 }
 
 // From https://stackoverflow.com/a/6234804/76472
