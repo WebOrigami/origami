5924a42504532b300cd0407b4fde5c9d2ba76194
Jan Miksovsky <jan@miksovsky.com>
2023-03-04 16:43:23 -0800

    Introduce siteHttp for exploring HTTP sites

--- src/builtins/siteHttp.js
+++ src/builtins/siteHttp.js
@@ -1,13 +1,13 @@
 import SiteGraph from "../core/SiteGraph.js";
 
-export default async function site(domain, ...keys) {
+export default async function siteHttp(domain, ...keys) {
   let url = [domain, ...keys].join("/");
   if (!url.startsWith("https") || !url.startsWith("http")) {
     if (!url.startsWith("//")) {
       url = `//${url}`;
     }
     if (!url.startsWith("http")) {
-      url = `https:${url}`;
+      url = `http:${url}`;
     }
   }
   if (!url.endsWith("/")) {
@@ -16,5 +16,5 @@ export default async function site(domain, ...keys) {
   return new SiteGraph(url);
 }
 
-site.usage = `site <domain>, <...keys>\tA web site graph via HTTPS`;
-site.documentation = "https://graphorigami.org/cli/builtins.html#site";
+siteHttp.usage = `siteHttp <domain>, <...keys>\tA web site graph via HTTP`;
+siteHttp.documentation = "https://graphorigami.org/cli/builtins.html#siteHttp";
