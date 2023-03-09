6b08882e8aecc0306fa8021f4da6149aa21219bd
Jan Miksovsky <jan@miksovsky.com>
2023-03-08 16:43:16 -0800

    Add scope adjustments to mergeDeep() like merge()

--- src/builtins/mergeDeep.js
+++ src/builtins/mergeDeep.js
@@ -1,7 +1,46 @@
 import MergeDeepGraph from "../common/MergeDeepGraph.js";
+import Scope from "../common/Scope.js";
+import ExplorableGraph from "../core/ExplorableGraph.js";
+import assertScopeIsDefined from "../language/assertScopeIsDefined.js";
 
+/**
+ * Create a graph that's the result of deep merging the given graphs.
+ *
+ * @this {Explorable}
+ * @param {GraphVariant[]} graphs
+ */
 export default async function mergeDeep(...graphs) {
-  return new MergeDeepGraph(...graphs);
+  assertScopeIsDefined(this);
+  // Filter out null or undefined graphs.
+  const filtered = graphs.filter((graph) => graph);
+
+  if (filtered.length === 1) {
+    // Only one graph, no need to merge.
+    return filtered[0];
+  }
+
+  // If a graph can take a scope, give it one that includes the other graphs and
+  // the current scope.
+  const scopedGraphs = filtered.map((graph) => {
+    let scopedGraph = ExplorableGraph.isExplorable(graph)
+      ? Object.create(/** @type {any} */ (graph))
+      : ExplorableGraph.from(graph);
+    if ("parent" in scopedGraph) {
+      const otherGraphs = graphs.filter((g) => g !== graph);
+      const scope = new Scope(...otherGraphs, this);
+      scopedGraph.parent = scope;
+    }
+    return scopedGraph;
+  });
+
+  // Merge the graphs.
+  const result = new MergeDeepGraph(...scopedGraphs);
+
+  // Give the overall mixed graph a scope that includes the component graphs and
+  // the current scope.
+  /** @type {any} */ (result).scope = new Scope(result, this);
+
+  return result;
 }
 
 mergeDeep.usage = `mergeDeep <...graphs>\tMerge the given graphs deeply`;
