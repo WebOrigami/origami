2c8ec653de317825e5f27d49061c7dbc718f0a1c
Jan Miksovsky <jan@miksovsky.com>
2023-03-19 20:51:56 -0700

    Use an async generator to crawl the paths, handling each result immediately as it comes in

--- src/builtins/crawl.js
+++ src/builtins/crawl.js
@@ -39,44 +39,23 @@ export default async function crawl(variant, baseHref) {
   const baseUrl = new URL(baseHref);
 
   const cache = {};
-  // Seed the queue with robots.txt and an empty path that will be equivalent to
-  // the base URL.
-  let pathQueue = ["/robots.txt", ""];
-  const seenPaths = new Set();
 
-  // We process the whole queue in parallel.
-  while (pathQueue.length > 0) {
-    const results = await Promise.all(
-      pathQueue.map((path) => processPath(graph, path, baseUrl))
-    );
+  // We iterate until there are no more promises to wait for.
+  for await (const result of crawlPaths(graph, baseUrl)) {
+    const { keys, resourcePaths, value } = result;
 
-    // Clear the queue.
-    pathQueue = [];
+    // Cache the value
+    if (value) {
+      addValueToObject(cache, keys, value);
+    }
 
-    // Process the results.
-    for (const result of results) {
-      const { crawlablePaths, keys, resourcePaths, value } = result;
-
-      // Cache the value
-      if (value) {
-        addValueToObject(cache, keys, value);
-      }
-
-      // Add new paths to the queue.
-      const newPaths = new Set(
-        crawlablePaths.filter((path) => !seenPaths.has(path))
-      );
-      pathQueue.push(...newPaths);
-      newPaths.forEach((path) => seenPaths.add(path));
-
-      // Add indirect resource references to the cache.
-      for (const resourcePath of resourcePaths) {
-        const fn = () => {
-          return ExplorableGraph.traversePath(graph, resourcePath);
-        };
-        const resourceKeys = keysFromPath(resourcePath);
-        addValueToObject(cache, resourceKeys, fn);
-      }
+    // Add indirect resource references to the cache.
+    for (const resourcePath of resourcePaths) {
+      const fn = () => {
+        return ExplorableGraph.traversePath(graph, resourcePath);
+      };
+      const resourceKeys = keysFromPath(resourcePath);
+      addValueToObject(cache, resourceKeys, fn);
     }
   }
 
@@ -111,6 +90,55 @@ function addValueToObject(object, keys, value) {
       }
       current = current[key];
     }
+  }
+}
+
+// Crawl the paths for the given graph, starting at the given base URL, and
+// yield the results. The results will include the HTML/script/stylesheet value
+// retrieved at a path, along with the paths to other resources found in that
+// text.
+async function* crawlPaths(graph, baseUrl) {
+  // We want to kick off requests for new paths as quickly as we find them, then
+  // yield whichever result finishes first. Unfortunately, Promise.any() only
+  // tells us the result of the first promise to resolve, not which promise that
+  // was. So we keep track of a dictionary mapping paths to a promise for the
+  // value at that path. When a promise resolves, we mark it as resolved by
+  // setting its entry in the dictionary to null.
+  const promisesForPaths = {};
+
+  // Seed the promise dictionary with robots.txt and an empty path that will be
+  // equivalent to the base URL.
+  const initialPaths = ["/robots.txt", ""];
+  initialPaths.forEach((path) => {
+    promisesForPaths[path] = processPath(graph, path, baseUrl);
+  });
+
+  while (true) {
+    // Get the latest array of promises that haven't been resolved yet.
+    const promises = Object.values(promisesForPaths).filter(
+      (promise) => promise !== null
+    );
+
+    if (promises.length === 0) {
+      // No unresolved promises; we're done.
+      break;
+    }
+
+    // Wait for the first promise to resolve.
+    const result = await Promise.any(promises);
+
+    // Mark the promise for that result as resolved.
+    promisesForPaths[result.path] = null;
+
+    // Add promises for crawlable paths in the result.
+    result.crawlablePaths.forEach((path) => {
+      // Only add a promise for this path if we don't already have one.
+      if (promisesForPaths[path] === undefined) {
+        promisesForPaths[path] = processPath(graph, path, baseUrl);
+      }
+    });
+
+    yield result;
   }
 }
 
@@ -153,6 +181,8 @@ function findPaths(value, key, baseUrl, localPath) {
     foundPaths = findPathsInCss(String(value));
   } else if (ext === ".js") {
     foundPaths = findPathsInJs(String(value));
+  } else if (ext === ".map") {
+    foundPaths = findPathsInImageMap(String(value));
   } else if (key === "robots.txt") {
     foundPaths = findPathsInRobotsTxt(String(value));
   } else if (key === "sitemap.xml") {
@@ -191,6 +221,26 @@ function findPathsInCss(css) {
   const urlRegex = /url\(["']?(?<url>[^"')]*?)["']?\)/g;
   while ((match = urlRegex.exec(css))) {
     resourcePaths.push(match.groups?.url);
+  }
+
+  return {
+    crawlablePaths: [],
+    resourcePaths,
+  };
+}
+
+// These are ancient server-side image maps. They're so old that it's hard to
+// find documentation on them, but they're used on the reference Space Jam
+// website we use for testing the crawler. Example:
+// https://www.spacejam.com/1996/bin/bball.map
+function findPathsInImageMap(imageMap) {
+  const resourcePaths = [];
+  let match;
+
+  // Find hrefs as the second column in each line.
+  const hrefRegex = /^\w+ (?<href>\S+)(\s*$| [\d, ]+$)/gm;
+  while ((match = hrefRegex.exec(imageMap))) {
+    resourcePaths.push(match.groups?.href);
   }
 
   return {
@@ -318,17 +368,30 @@ function isCrawlableHref(href) {
   }
   const ext = extname(lastKey);
   // We assume an empty extension is HTML.
-  const crawlableExtensions = [".html", ".css", ".js", ""];
+  const crawlableExtensions = [".html", ".css", ".js", ".map", ""];
   return crawlableExtensions.includes(ext);
 }
 
 async function processPath(graph, path, baseUrl) {
   if (path === undefined) {
-    return { crawlablePaths: [], keys: null, resourcePaths: [], value: null };
+    return {
+      crawlablePaths: [],
+      keys: null,
+      path,
+      resourcePaths: [],
+      value: null,
+    };
   }
 
-  // Get value at path
-  const keys = keysFromPath(path);
+  // Convert path to keys
+  /** @type {any[]} */
+  let keys = keysFromPath(path);
+  if (keys.length === 0) {
+    // Root path
+    keys = [undefined];
+  }
+
+  // Traverse graph to get value.
   let value = await ExplorableGraph.traverse(graph, ...keys);
   if (ExplorableGraph.isExplorable(value)) {
     // Path is actually a directory; see if it has an index.html
@@ -340,7 +403,7 @@ async function processPath(graph, path, baseUrl) {
   }
 
   if (value === undefined) {
-    return { crawlablePaths: [], keys, resourcePaths: [], value: null };
+    return { crawlablePaths: [], keys, path, resourcePaths: [], value: null };
   }
 
   if (keys.at(-1) === undefined) {
@@ -359,7 +422,7 @@ async function processPath(graph, path, baseUrl) {
     path
   );
 
-  return { crawlablePaths, keys, resourcePaths, value };
+  return { crawlablePaths, keys, path, resourcePaths, value };
 }
 
 crawl.usage = `crawl <graph>\tCrawl a graph`;
