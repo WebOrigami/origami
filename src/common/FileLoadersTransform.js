f63de7a95f06218b9dd45ef8239f4fdf8d96f2fd
Jan Miksovsky <jan@miksovsky.com>
2023-04-05 16:44:17 -0700

    Pass the loader function the graph's scope, not the graph itself, as the scope the loader should use

--- src/common/FileLoadersTransform.js
+++ src/common/FileLoadersTransform.js
@@ -16,16 +16,16 @@ export default function FileLoadersTransform(Base) {
       if (value && typeof key === "string") {
         const extension = extname(key).toLowerCase().slice(1);
         if (extension) {
+          const scope = getScope(this);
           if (!this.loaders) {
             // Give the scope a chance to contribute loaders, otherwise fall
             // back to the built-in loaders.
-            const scope = getScope(this);
             const customLoaders = await scope.get("@loaders");
             this.loaders = customLoaders ?? builtinLoaders;
           }
           const loader = await this.loaders.get(extension);
           if (loader) {
-            value = await loader.call(this, value, key);
+            value = await loader.call(scope, value, key);
           }
         }
       }
