8cb6651665e0d839a66559beebd4b943f49815a4
Jan Miksovsky <jan@miksovsky.com>
2023-04-21 10:27:38 -0700

    Another unit test

--- test/common/GlobGraph.test.js
+++ test/common/GlobGraph.test.js
@@ -1,4 +1,5 @@
 import GlobGraph from "../../src/common/GlobGraph.js";
+import ExplorableGraph from "../../src/core/ExplorableGraph.js";
 import assert from "../assert.js";
 
 describe("GlobGraph", () => {
@@ -15,5 +16,22 @@ describe("GlobGraph", () => {
     assert(await globGraph.get("foobarbaz"));
     assert(!(await globGraph.get("foobaz")));
     assert(!(await globGraph.get("foo")));
+  });
+
+  it("matches nested globs", async () => {
+    const globGraph = new GlobGraph({
+      sub: {
+        foo: "bar",
+        "*": "default",
+      },
+    });
+    assert.equal(
+      await ExplorableGraph.traverse(globGraph, "sub", "file"),
+      "default"
+    );
+    assert.equal(
+      await ExplorableGraph.traverse(globGraph, "sub", "foo"),
+      "bar"
+    );
   });
 });
