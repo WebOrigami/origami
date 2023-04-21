650e7eaf528d22d9d315395386686a08ed99983b
Jan Miksovsky <jan@miksovsky.com>
2023-04-21 10:27:30 -0700

    Add @equals builtin

---
+++ src/builtins/@equals.js
@@ -0,0 +1,6 @@
+export default function equals(a, b) {
+  return a === b;
+}
+
+equals.usage = `@equals <a>, <b>\tTrue if a strictly equals b`;
+equals.documentation = "https://graphorigami.org/language/@equals.html";
