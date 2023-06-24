3fc5cd8d507e832e15e554becd39f6b7d6dd617f
Jan Miksovsky <jan@miksovsky.com>
2023-06-23 21:46:19 -0700

    Try using new core ObjectGraph as Origami ObjectGraph

---
+++ packages/core/exports.js
@@ -0,0 +1,12 @@
+export { default as DictionaryHelpers } from "./src/DictionaryHelpers.js";
+export { default as FilesGraph } from "./src/FilesGraph.js";
+export { default as FunctionDictionary } from "./src/FunctionDictionary.js";
+export { default as FunctionGraph } from "./src/FunctionGraph.js";
+export { default as GraphHelpers } from "./src/GraphHelpers.js";
+export { default as MapDictionary } from "./src/MapDictionary.js";
+export { default as MapGraph } from "./src/MapGraph.js";
+export { default as ObjectDictionary } from "./src/ObjectDictionary.js";
+export { default as ObjectGraph } from "./src/ObjectGraph.js";
+export { default as SetDictionary } from "./src/SetDictionary.js";
+export { default as SetGraph } from "./src/SetGraph.js";
+export { default as SiteGraph } from "./src/SiteGraph.js";
---
+++ packages/origami/foo.js
@@ -0,0 +1,9 @@
+import { ObjectGraph } from "@graphorigami/core";
+
+const graph = new ObjectGraph({
+  a: "Alice",
+  b: "Bob",
+  c: "Carol",
+});
+
+export default graph;
