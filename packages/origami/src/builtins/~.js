5fddd0ac0a1aacbeb14a1f4b559efe5adc3d2789
Jan Miksovsky <jan@miksovsky.com>
2023-09-13 11:26:02 -0700

    ~ built-in applies FileTreeTransform

--- packages/origami/src/builtins/~.js
+++ packages/origami/src/builtins/~.js
@@ -1,4 +1,5 @@
 import { FilesGraph } from "@graphorigami/core";
 import os from "node:os";
+import FileTreeTransform from "../framework/FileTreeTransform.js";
 
-export default new FilesGraph(os.homedir());
+export default new (FileTreeTransform(FilesGraph))(os.homedir());
