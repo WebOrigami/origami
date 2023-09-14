e5e45ab385027f8e96021f7c975f3bde14df59e3
Jan Miksovsky <jan@miksovsky.com>
2023-09-14 16:38:36 -0700

    Drop ImplicitModulesTransform from OrigamiFiles. This deprecates the ability to reference a function in a JavaScript file foo.js in an Origami expression as foo(). The function must now be referenced as foo.js().

--- packages/origami/src/framework/OrigamiFiles.d.ts
+++ packages/origami/src/framework/OrigamiFiles.d.ts
@@ -1,12 +1,11 @@
 import { FilesGraph } from "@graphorigami/core";
 import EventTargetMixin from "./EventTargetMixin.js";
 import FileTreeTransform from "./FileTreeTransform.js";
-import ImplicitModulesTransform from "./ImplicitModulesTransform.js";
 import ImportModulesMixin from "./ImportModulesMixin.js";
 import WatchFilesMixin from "./WatchFilesMixin.js";
 
 export default class OrigamiFiles extends FileTreeTransform(
-  ImplicitModulesTransform(
+  (
     ImportModulesMixin(WatchFilesMixin(EventTargetMixin(FilesGraph)))
   )
 ) {}
