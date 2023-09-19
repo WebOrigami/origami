2cfa5a1e018e94f092b9e00ca29c2800df1dd615
Jan Miksovsky <jan@miksovsky.com>
2023-09-18 17:24:28 -0700

    Simplify experimental .ori loader

--- packages/origami/src/loaders/ori2.js
+++ packages/origami/src/loaders/ori2.js
@@ -1,10 +1,9 @@
 /** @typedef {import("@graphorigami/types").AsyncDictionary} AsyncDictionary */
-import { FunctionGraph, Graph, ObjectGraph } from "@graphorigami/core";
+import { Graph } from "@graphorigami/core";
 import DeferredGraph from "../common/DeferredGraph.js";
 import StringWithGraph from "../common/StringWithGraph.js";
 import { getScope, graphInContext, keySymbol } from "../common/utilities.js";
 import * as compile from "../language/compile.js";
-import execute from "../language/execute.js";
 import * as ops from "../language/ops.js";
 
 /**
@@ -14,28 +13,26 @@ import * as ops from "../language/ops.js";
  * @param {any} [key]
  * @this {AsyncDictionary|null}
  */
-export default function loadGraph(buffer, key) {
+export default function loadOrigamiExpression(buffer, key) {
   const text = String(buffer);
   const scope = getScope(this);
   const deferredGraph = new DeferredGraph(async () => {
     const fn = compile.expression(text);
     let value = await fn.call(scope);
-    // Return graphs as is, wrap functions in a FunctionGraph, and wrap anything
-    // else in a a graph whose default value is the expression's value.
-    if (fn.code[0] === ops.lambda && fn.code[1][0] === ops.concat) {
-      // HACK: Create cheap version of what Template does
-      const func = async (input) => {
-        const extendedScope = Graph.isGraphable(input)
-          ? graphInContext(input, scope)
-          : scope;
-        const template = fn.code[1];
-        return await execute.call(extendedScope, template);
-      };
-      value = new FunctionGraph(func);
-    } else if (typeof value === "function") {
-      value = new FunctionGraph(fn.bind(scope));
-    } else if (!Graph.isAsyncDictionary(value)) {
-      value = new ObjectGraph({ "": value });
+    // If value is a function, bind it to the file container's scope.
+    if (typeof value === "function") {
+      // HACK: Is the function for a template literal?
+      if (fn.code[0] === ops.lambda && fn.code[1][0] === ops.concat) {
+        // Yes: create cheap version of what Template does.
+        const templateFn = value;
+        value = async (input) => {
+          const extendedScope = Graph.isGraphable(input)
+            ? graphInContext(input, this)
+            : this;
+          return templateFn.call(extendedScope);
+        };
+      }
+      value = value.bind(scope);
     }
     value[keySymbol] = key;
     return value;
