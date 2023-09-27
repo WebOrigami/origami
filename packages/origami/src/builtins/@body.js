eb6b14d0a1be748bbe88cea89fefdc6f94a07efc
Jan Miksovsky <jan@miksovsky.com>
2023-09-27 09:40:18 -0700

    @body passes along input contents if defined

--- packages/origami/src/builtins/@body.js
+++ packages/origami/src/builtins/@body.js
@@ -1,8 +1,11 @@
+import TextWithContents from "../common/TextWithContents.js";
 import { extractFrontMatter } from "../common/serialize.js";
 
 export default function body(input) {
   const { bodyText } = extractFrontMatter(input);
-  return bodyText;
+  return input.contents
+    ? new TextWithContents(bodyText, input.contents)
+    : bodyText;
 }
 
 body.usage = `@body <text>\tThe body of the text without any front matter`;
