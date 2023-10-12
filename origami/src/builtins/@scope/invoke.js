df8a2d604a9a8e1b667fe5963b47dd0b447554d0
Jan Miksovsky <jan@miksovsky.com>
2023-10-12 15:19:31 -0700

    @scope/invoke gets scope of first argument, doesn't use that argument directly

--- origami/src/builtins/@scope/invoke.js
+++ origami/src/builtins/@scope/invoke.js
@@ -1,4 +1,4 @@
-import { toFunction } from "../../common/utilities.js";
+import { getScope, toFunction } from "../../common/utilities.js";
 
 /**
  * Invokes the given function in the context of the current scope.
@@ -7,10 +7,11 @@ import { toFunction } from "../../common/utilities.js";
  * @typedef {import("../../..").Invocable} Invocable
  *
  * @this {AsyncDictionary|null}
- * @param {AsyncDictionary} scope
+ * @param {AsyncDictionary} context
  * @param {Invocable} invocable
  */
-export default async function invoke(scope, invocable, ...args) {
+export default async function invoke(context, invocable, ...args) {
+  const scope = getScope(context);
   const fn = toFunction(invocable);
   const result = await fn.call(scope, ...args);
   return result;
