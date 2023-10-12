f56b387c381576f76198dbd836520fc294bfc175
Jan Miksovsky <jan@miksovsky.com>
2023-10-12 10:28:30 -0700

    @files built-in returns OrigamiFiles instance so it can be used to do things like module imports

--- origami/src/builtins/@files.js
+++ origami/src/builtins/@files.js
@@ -1,9 +1,8 @@
 /** @typedef {import("@graphorigami/types").AsyncDictionary} AsyncDictionary */
-import { FilesGraph } from "@graphorigami/core";
 import path from "node:path";
 import process from "node:process";
-import FileTreeTransform from "../framework/FileTreeTransform.js";
 import assertScopeIsDefined from "../language/assertScopeIsDefined.js";
+import OrigamiFiles from "../framework/OrigamiFiles.js";
 
 /**
  * @this {AsyncDictionary|null}
@@ -12,7 +11,7 @@ import assertScopeIsDefined from "../language/assertScopeIsDefined.js";
 export default async function files(...keys) {
   assertScopeIsDefined(this);
   const resolved = path.resolve(process.cwd(), ...keys);
-  return new (FileTreeTransform(FilesGraph))(resolved);
+  return new OrigamiFiles(resolved);
 }
 
 files.usage = `@files [path]\tGraph of files at the given path`;
