9a547c0f2c96b2c7ee6e5f1251de39ba8f2cd9d3
Jan Miksovsky <jan@miksovsky.com>
2023-10-12 17:35:32 -0700

    Take care to avoid reusing the attachedData directly

--- origami/src/common/processUnpackedContent.js
+++ origami/src/common/processUnpackedContent.js
@@ -2,7 +2,12 @@ import { Graph } from "@graphorigami/core";
 import builtins from "../builtins/@builtins.js";
 import InheritScopeTransform from "../framework/InheritScopeTransform.js";
 import Scope from "./Scope.js";
-import { getScope, isTransformApplied, transformObject } from "./utilities.js";
+import {
+  getScope,
+  graphInContext,
+  isTransformApplied,
+  transformObject,
+} from "./utilities.js";
 
 /**
  * Perform any necessary post-processing on the unpacked content of a file. This
@@ -24,12 +29,8 @@ export default function processUnpackedContent(content, parent, attachedData) {
     function extendScope(input, ...rest) {
       let attachedGraph;
       if (attachedData) {
-        attachedGraph = Graph.from(attachedData);
-        if (!isTransformApplied(InheritScopeTransform, attachedGraph)) {
-          attachedGraph = transformObject(InheritScopeTransform, attachedGraph);
-        }
-        let attachedAmbients = input ? { _: input } : null;
-        attachedGraph.parent = new Scope(attachedAmbients, parentScope);
+        const ambientGraph = graphInContext({ _: input }, parent);
+        attachedGraph = graphInContext(attachedData, ambientGraph);
       }
 
       const baseScope = this ?? builtins;
