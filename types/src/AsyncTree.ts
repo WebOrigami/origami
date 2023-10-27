c770c06bca5673dd917c995d27024718ec2d2a08
Jan Miksovsky <jan@miksovsky.com>
2023-10-26 21:59:35 -0700

    Comments

--- types/src/AsyncTree.ts
+++ types/src/AsyncTree.ts
@@ -1,7 +1,7 @@
 /**
  * A read-only async dictionary that can represent a node in a tree.
  *
- * Implicit in this interface: a tree often returns values that also trees,
+ * Implicit in this interface: a tree often returns values that are also trees,
  * typically the same kind of tree, but that is not required.
  */
 export default interface AsyncTree {
