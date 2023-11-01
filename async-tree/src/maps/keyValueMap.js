3890ea1a6dcd4c7295aa4776813e161a664b2d7e
Jan Miksovsky <jan@miksovsky.com>
2023-11-01 15:14:05 -0700

    Rewriting for clarification

--- async-tree/src/maps/keyValueMap.js
+++ async-tree/src/maps/keyValueMap.js
@@ -16,46 +16,52 @@ export default function keyValueMap({
    */
   return function applyKeyValueMap(tree) {
     const mapped = Object.create(tree);
-    return Object.assign(
-      mapped,
-      (innerKeyFn || keyFn) &&
-        valueFn && {
-          description,
+    mapped.description = description;
 
-          async get(outerKey) {
-            const innerKey =
-              innerKeyFn?.(outerKey) ?? keyFn
-                ? await defaultInnerKeyFn(tree, keyFn, outerKey)
-                : outerKey;
-            if (!innerKey) {
-              return undefined;
-            }
-            const innerValue = await tree.get(innerKey);
-            const outerValue = Tree.isAsyncTree(innerValue)
-              ? // Apply map to tree value
-                keyValueMap({
-                  description,
-                  innerKeyFn,
-                  keyFn,
-                  valueFn,
-                })(innerValue)
-              : valueFn?.(innerValue) ?? innerValue;
-            return outerValue;
-          },
-        },
+    if (valueFn) {
+      mapped.get = async (outerKey) => {
+        // Step 1: Map the outer key to the inner key.
+        let innerKey;
+        if (innerKeyFn) {
+          innerKey = await innerKeyFn(outerKey);
+        } else if (keyFn) {
+          innerKey = await slowInverseKeyFn(tree, keyFn, outerKey);
+        } else {
+          innerKey = outerKey;
+        }
 
-      keyFn && {
-        async keys() {
-          const innerKeys = [...(await tree.keys())];
-          const outerKeys = innerKeys.map(keyFn);
-          return outerKeys;
-        },
-      }
-    );
+        if (!innerKey) {
+          // No inner key means no value.
+          return undefined;
+        }
+
+        // Step 2: Get the inner value.
+        const innerValue = await tree.get(innerKey);
+
+        // Step 3: Map the inner value to the outer value.
+        const mapFn = Tree.isAsyncTree(innerValue)
+          ? applyKeyValueMap // Map a subtree.
+          : valueFn; // Map a single value.
+        const outerValue = await mapFn(innerValue);
+
+        return outerValue;
+      };
+    }
+
+    if (keyFn) {
+      mapped.keys = async () => {
+        const innerKeys = [...(await tree.keys())];
+        const outerKeys = innerKeys.map(keyFn);
+        return outerKeys;
+      };
+    }
+
+    return mapped;
   };
 }
 
-async function defaultInnerKeyFn(tree, keyFn, outerKey) {
+// Enumerate all the inner keys and return the one that maps to the outer key.
+async function slowInverseKeyFn(tree, keyFn, outerKey) {
   const innerKeys = [...(await tree.keys())];
   const innerKey = innerKeys.find((key) => keyFn(key) === outerKey);
   return innerKey;
