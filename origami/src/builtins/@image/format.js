136aab436fca89b0e65186725ee611ac3961e327
Jan Miksovsky <jan@miksovsky.com>
2024-02-08 11:40:21 -0800

    Functional forms for @image/format and @image/resize

--- origami/src/builtins/@image/format.js
+++ origami/src/builtins/@image/format.js
@@ -1,5 +1,39 @@
 import sharp from "sharp";
 
-export default function format(buffer, format, options) {
-  return sharp(buffer).toFormat(format, options).toBuffer();
+/**
+ * Return the image in a different format.
+ *
+ * @this {import("@weborigami/types").AsyncTree|null}
+ *
+ * @typedef {import("sharp").ResizeOptions} ResizeOptions
+ *
+ * @overload
+ * @param {any} param1
+ * @param {any} param2
+ * @returns {(buffer: Buffer) => Promise<Buffer>}
+ *
+ * @overload
+ * @param {Buffer} param1
+ * @param {any} param2
+ * @param {any} param3
+ * @returns {Promise<Buffer>}
+ */
+export default function format(param1, param2, param3) {
+  // Identify which overload was used.
+  let buffer;
+  let format;
+  let options;
+  if (param1 instanceof Buffer) {
+    buffer = param1;
+    format = param2;
+    options = param3;
+  } else {
+    format = param1;
+    options = param2;
+  }
+
+  const transform = (buffer) =>
+    sharp(buffer).toFormat(format, options).toBuffer();
+
+  return buffer ? transform(buffer) : transform;
 }
--- origami/src/builtins/@image/resize.js
+++ origami/src/builtins/@image/resize.js
@@ -1,5 +1,33 @@
 import sharp from "sharp";
 
-export default function resize(buffer, options) {
-  return sharp(buffer).resize(options).toBuffer();
+/**
+ * Resize an image.
+ *
+ * @this {import("@weborigami/types").AsyncTree|null}
+ *
+ * @typedef {import("sharp").ResizeOptions} ResizeOptions
+ *
+ * @overload
+ * @param {ResizeOptions} param1
+ * @returns {(buffer: Buffer) => Promise<Buffer>}
+ *
+ * @overload
+ * @param {Buffer} param1
+ * @param {ResizeOptions} param2
+ * @returns {Promise<Buffer>}
+ */
+export default function resize(param1, param2) {
+  // Identify which overload was used.
+  let buffer;
+  let options;
+  if (param2 === undefined) {
+    options = param1;
+  } else {
+    buffer = param1;
+    options = param2;
+  }
+
+  const transform = (buffer) => sharp(buffer).resize(options).toBuffer();
+
+  return buffer ? transform(buffer) : transform;
 }
