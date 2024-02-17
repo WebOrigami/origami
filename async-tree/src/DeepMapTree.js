97e84bf3d7f8c7fe77362e952423c706a5d1194f
Jan Miksovsky <jan@miksovsky.com>
2024-02-16 17:01:01 -0800

    Introduce DeepMapTree

---
+++ async-tree/src/DeepMapTree.js
@@ -0,0 +1,23 @@
+import MapTree from "./MapTree.js";
+import * as Tree from "./Tree.js";
+
+export default class DeepMapTree extends MapTree {
+  async get(key) {
+    let value = await super.get(key);
+
+    if (value instanceof Map) {
+      value = Reflect.construct(this.constructor, [value]);
+    }
+
+    if (Tree.isAsyncTree(value) && !value.parent) {
+      value.parent = this;
+    }
+
+    return value;
+  }
+
+  async isKeyForSubtree(key) {
+    const value = this.map.get[key];
+    return value instanceof Map || Tree.isAsyncTree(value);
+  }
+}
