87f4c81048696c0d767b2af184138c36a844ef61
Jan Miksovsky <jan@miksovsky.com>
2024-02-19 15:56:07 -0800

    Deprecate @tree/fromJson and @tree/fromYaml, can use @tree/from + @parse/json or @parse/yaml instead

--- origami/src/builtins/@tree/fromJson.js
+++
@@ -1,6 +0,0 @@
-export default async function fromJson(text) {
-  return text ? JSON.parse(text) : undefined;
-}
-
-fromJson.usage = `fromJson <text>\tParse text as JSON`;
-fromJson.documentation = "https://weborigami.org/cli/builtins.html#fromJson";
