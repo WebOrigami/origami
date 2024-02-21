46e95a0186d71771476bd2a6da00e8ed828f2ae2
Jan Miksovsky <jan@miksovsky.com>
2024-02-21 10:04:06 -0800

    Typo

--- origami/src/misc/assertScopeIsDefined.js
+++ origami/src/misc/assertScopeIsDefined.js
@@ -1,7 +1,7 @@
 export default function assertScopeIsDefined(scope) {
   if (scope === undefined) {
     throw new Error(
-      "Tree methods must be called a scope. If you don't want to pass a scope, invoke with: <methodName>.call(null)"
+      "Tree methods must be called with a scope. If you don't want to pass a scope, invoke with: <methodName>.call(null)"
     );
   }
 }
