7ff33a84546c987136eb9beed02be01780f3433f
Jan Miksovsky <jan@miksovsky.com>
2024-03-05 14:41:23 -0800

    Patch process.env to return a plain object

--- origami/src/builtins/@node.js
+++ origami/src/builtins/@node.js
@@ -2,10 +2,21 @@ import path from "node:path";
 import process from "node:process";
 import url from "node:url";
 
+// Patch process.env to be a plain object. Among other things, this lets us dump
+// the complete environment to the terminal with `ori @node/process/env`.
+const patchedProcess = Object.create(null, {
+  ...Object.getOwnPropertyDescriptors(process),
+  env: {
+    value: function () {
+      return Object.assign({}, process.env);
+    },
+  },
+});
+
 const node = {
   Buffer,
   path,
-  process,
+  process: patchedProcess,
   url,
 };
 
