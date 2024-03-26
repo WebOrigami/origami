1ff65d580cb25b64e38db0bf300c68f4e73a7516
Jan Miksovsky <jan@miksovsky.com>
2024-03-26 11:05:26 -0700

    documentObject() input can be an existing document object or a string

--- origami/src/common/documentObject.js
+++ origami/src/common/documentObject.js
@@ -1,4 +1,4 @@
-import { symbols } from "@weborigami/async-tree";
+import { isPlainObject, symbols } from "@weborigami/async-tree";
 import txtHandler from "../builtins/txt_handler.js";
 
 /**
@@ -6,10 +6,24 @@ import txtHandler from "../builtins/txt_handler.js";
  * a pack() method that formats that object as text with YAML front matter. This
  * function is a helper for constructing such text document objects.
  *
- * The `text` input will be coerced to a string.
+ * @typedef {import("@weborigami/async-tree").StringLike} StringLike
+ * @typedef {import("@weborigami/async-tree").PlainObject} PlainObject
+ *
+ * @param {StringLike|PlainObject} input
+ * @param {PlainObject} [data]
  */
-export default function documentObject(text, data) {
-  const result = Object.assign({}, data, { "@text": String(text) });
+export default function documentObject(input, data) {
+  let text;
+  let inputData;
+  if (isPlainObject(input)) {
+    text = input["@text"];
+    inputData = input;
+  } else {
+    text = String(input);
+    inputData = null;
+  }
+  const result = Object.assign({}, inputData, data, { "@text": text });
+  // @ts-ignore
   result[symbols.pack] = () => txtHandler.pack(result);
   return result;
 }
