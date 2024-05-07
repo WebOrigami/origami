8d462573122736e50ab4dc711a5a70bdc9dad911
Jan Miksovsky <jan@miksovsky.com>
2024-05-06 17:27:07 -0700

    Generalize @tree from a specific function that casts its treelike argument to a tree to the general-purpose collection of utility function in async-tree's Tree module. Old: @tree, new: @tree/from. Drop @isAsyncTree, which is now @tree/isAsyncTree.

--- origami/src/builtins/@tree.js
+++ origami/src/builtins/@tree.js
@@ -1,16 +1,4 @@
-import getTreeArgument from "../misc/getTreeArgument.js";
-
 /**
- * Cast the indicated treelike to a tree.
- *
- * @typedef  {import("@weborigami/types").AsyncTree} AsyncTree
- * @typedef {import("@weborigami/async-tree").Treelike} Treelike
- * @this {AsyncTree|null}
- * @param {Treelike} [treelike]
+ * Tree utilities functions
  */
-export default async function tree(treelike) {
-  return getTreeArgument(this, arguments, treelike, "@tree");
-}
-
-tree.usage = `@tree <treelike>\tConvert JSON, YAML, function, or plain object to a tree`;
-tree.documentation = "https://weborigami.org/cli/builtins.html#tree";
+export { Tree as default } from "@weborigami/async-tree";
