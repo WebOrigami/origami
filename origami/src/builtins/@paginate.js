3232930234e10193d321cf34f7f628c1ec1e2e40
Jan Miksovsky <jan@miksovsky.com>
2024-05-10 16:20:35 -0700

    Introduce @paginateFn functional form of @paginate

--- origami/src/builtins/@paginate.js
+++ origami/src/builtins/@paginate.js
@@ -1,4 +1,5 @@
 import getTreeArgument from "../misc/getTreeArgument.js";
+import paginateFn from "./@paginateFn.js";
 
 /**
  * Return a new grouping of the treelike's values into chunks of the specified
@@ -6,41 +7,12 @@ import getTreeArgument from "../misc/getTreeArgument.js";
  *
  * @typedef {import("@weborigami/types").AsyncTree} AsyncTree
  * @typedef {import("@weborigami/async-tree").Treelike} Treelike
+ *
  * @this {AsyncTree|null}
  * @param {Treelike} [treelike]
  * @param {number} [size=10]
  */
 export default async function paginate(treelike, size = 10) {
   const tree = await getTreeArgument(this, arguments, treelike, "@count");
-  const keys = Array.from(await tree.keys());
-  const pageCount = Math.ceil(keys.length / size);
-  return {
-    async get(pageKey) {
-      const pageNumber = Number(pageKey);
-      if (Number.isNaN(pageNumber)) {
-        return undefined;
-      }
-      const nextKey = pageNumber + 1 < pageCount ? pageNumber + 1 : null;
-      const previousKey = pageNumber - 1 >= 0 ? pageNumber - 1 : null;
-      const items = {};
-      for (
-        let index = pageNumber * size;
-        index < Math.min(keys.length, (pageNumber + 1) * size);
-        index++
-      ) {
-        const key = keys[index];
-        items[key] = await tree.get(keys[index]);
-      }
-      return {
-        items,
-        nextKey,
-        previousKey,
-      };
-    },
-
-    async keys() {
-      // Return an array from 1..pageCount
-      return Array.from({ length: pageCount }, (_, index) => index);
-    },
-  };
+  return paginateFn.call(this, size)(tree);
 }
--- origami/test/builtins/@paginateFn.test.js
+++ origami/test/builtins/@paginateFn.test.js
@@ -1,9 +1,9 @@
 import { Tree } from "@weborigami/async-tree";
 import assert from "node:assert";
 import { describe, test } from "node:test";
-import paginate from "../../src/builtins/@paginate.js";
+import paginateFn from "../../src/builtins/@paginateFn.js";
 
-describe("@paginate", () => {
+describe("@paginateFn", () => {
   test("divides tree keys into fixed-length chunks", async () => {
     const treelike = {
       a: 1,
@@ -12,7 +12,7 @@ describe("@paginate", () => {
       d: 4,
       e: 5,
     };
-    const paginated = await paginate.call(null, treelike, 2);
+    const paginated = await paginateFn.call(null, 2)(treelike);
     assert.deepEqual(await Tree.plain(paginated), [
       {
         items: { a: 1, b: 2 },
