063ae740e365fce272365c5d4404fe85561f15b9
Jan Miksovsky <jan@miksovsky.com>
2024-06-08 10:32:13 -0700

    Typo

--- origami/src/builtins/@clean.js
+++ origami/src/builtins/@clean.js
@@ -8,7 +8,7 @@ import getTreeArgument from "../misc/getTreeArgument.js";
  * @param {import("@weborigami/async-tree").Treelike} treelike
  */
 export default async function clean(treelike) {
-  const tree = await getTreeArgument(this, arguments, treelike, "@sequence");
+  const tree = await getTreeArgument(this, arguments, treelike, "@clean");
   if (!Tree.isAsyncMutableTree(tree)) {
     throw new TypeError("@clean: the given tree is read-only.");
   }
