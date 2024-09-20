53af08bd28c501fbbd238e2bbc0dda9af33e0390
Jan Miksovsky <jan@miksovsky.com>
2024-09-20 15:01:41 -0700

    Deprecate jsonKeys.parse()

--- async-tree/src/jsonKeys.js
+++ async-tree/src/jsonKeys.js
@@ -10,32 +10,6 @@ import { Tree } from "./internal.js";
  */
 
 /**
- * Parse the JSON in a .keys.json file.
- *
- * This returns a flat dictionary of flags which are true for subtrees and
- * false otherwise.
- *
- * Example: the JSON `["index.html","about/"]` parses as:
- *
- *   {
- *     "index.html": false,
- *     about: true,
- *   }
- */
-export function parse(json) {
-  const descriptors = JSON.parse(json);
-  const result = {};
-  for (const descriptor of descriptors) {
-    if (descriptor.endsWith("/")) {
-      result[descriptor.slice(0, -1)] = true;
-    } else {
-      result[descriptor] = false;
-    }
-  }
-  return result;
-}
-
-/**
  * Given a tree node, return a JSON string that can be written to a .keys.json
  * file.
  */
--- async-tree/test/jsonKeys.test.js
+++ async-tree/test/jsonKeys.test.js
@@ -4,15 +4,6 @@ import { describe, test } from "node:test";
 import * as jsonKeys from "../src/jsonKeys.js";
 
 describe("jsonKeys", () => {
-  test("parses JSON Keys", async () => {
-    const json = '["index.html","about/"]';
-    const parsed = jsonKeys.parse(json);
-    assert.deepStrictEqual(parsed, {
-      "index.html": false,
-      about: true,
-    });
-  });
-
   test("stringifies JSON Keys", async () => {
     const tree = new DeepObjectTree({
       about: {},
