9f5e840b1f8a2bdc7ac556f31ba73af748f24b59
Jan Miksovsky <jan@miksovsky.com>
2024-10-09 16:39:35 -0700

    @changes normalizes keys for comparison

--- origami/src/builtins/@changes.js
+++ origami/src/builtins/@changes.js
@@ -1,4 +1,4 @@
-import { Tree } from "@weborigami/async-tree";
+import { trailingSlash, Tree } from "@weborigami/async-tree";
 
 /**
  * Given an old tree and a new tree, return a tree of changes indicated
@@ -17,41 +17,46 @@ export default async function changes(oldTreelike, newTreelike) {
   const oldKeys = Array.from(await oldTree.keys());
   const newKeys = Array.from(await newTree.keys());
 
+  const oldKeysNormalized = oldKeys.map(trailingSlash.remove);
+  const newKeysNormalized = newKeys.map(trailingSlash.remove);
+
   let result;
 
-  for (const key of oldKeys) {
-    if (!newKeys.includes(key)) {
+  for (const oldKey of oldKeys) {
+    const oldNormalized = trailingSlash.remove(oldKey);
+    if (!newKeysNormalized.includes(oldNormalized)) {
       result ??= {};
-      result[key] = "deleted";
+      result[oldKey] = "deleted";
       continue;
     }
 
-    const oldValue = await oldTree.get(key);
-    const newValue = await newTree.get(key);
+    const oldValue = await oldTree.get(oldKey);
+    const newValue = await newTree.get(oldKey);
 
     if (Tree.isAsyncTree(oldValue) && Tree.isAsyncTree(newValue)) {
       const treeChanges = await changes.call(this, oldValue, newValue);
       if (treeChanges && Object.keys(treeChanges).length > 0) {
         result ??= {};
-        result[key] = treeChanges;
+        result[oldKey] = treeChanges;
       }
     } else if (oldValue?.toString && newValue?.toString) {
       const oldText = oldValue.toString();
       const newText = newValue.toString();
       if (oldText !== newText) {
         result ??= {};
-        result[key] = "changed";
+        result[oldKey] = "changed";
       }
     } else {
       result ??= {};
-      result[key] = "changed";
+      result[oldKey] = "changed";
     }
   }
 
-  for (const key of newKeys) {
-    if (!oldKeys.includes(key)) {
+  for (const newKey of newKeys) {
+    const newNormalized = trailingSlash.remove(newKey);
+    if (!oldKeysNormalized.includes(newNormalized)) {
       result ??= {};
-      result[key] = "added";
+      result[newKey] = "added";
     }
   }
 
