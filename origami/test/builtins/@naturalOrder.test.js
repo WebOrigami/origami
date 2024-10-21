7501a0768f0da7e8939734d0a39bf20473c52f27
Jan Miksovsky <jan@miksovsky.com>
2024-10-21 16:24:55 -0700

    Add test

---
+++ origami/test/builtins/@naturalOrder.test.js
@@ -0,0 +1,17 @@
+import assert from "node:assert";
+import { describe, test } from "node:test";
+import naturalOrder from "../../src/builtins/@naturalOrder.js";
+import sort from "../../src/builtins/@sort.js";
+
+describe("@naturalOrder", () => {
+  test("sorts a tree's keys using natural sort order", async () => {
+    const tree = {
+      1: "one",
+      10: "ten",
+      2: "two",
+    };
+    const sorted = await sort.call(null, tree, naturalOrder);
+    const keys = await sorted.keys();
+    assert.deepEqual(keys, ["1", "2", "10"]);
+  });
+});
