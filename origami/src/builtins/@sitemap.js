d59aab362527f44febaca65fdc1294f47585b297
Jan Miksovsky <jan@miksovsky.com>
2024-10-21 16:24:42 -0700

    Format tweaks

--- origami/src/builtins/@sitemap.js
+++ origami/src/builtins/@sitemap.js
@@ -6,12 +6,10 @@ import fileTypeOrigami from "./ori_handler.js";
 
 const templateText = `(urls) => \`<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-\${ @map(urls, (url) => \`
-  <url>
+\${ @map(urls, (url) => \`  <url>
     <loc>\${ url }</loc>
   </url>
-\`) }
-</urlset>
+\`) }</urlset>
 \`
 `;
 
