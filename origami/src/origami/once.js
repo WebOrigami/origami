381db52da2be26e65122cf7e24ca46fec5611d22
Jan Miksovsky <jan@miksovsky.com>
2024-12-19 17:03:08 -0800

    Fix origami:once, which was probably broken when we stopped caching the functions returned by ops.lambda

--- origami/src/origami/once.js
+++ origami/src/origami/once.js
@@ -1,6 +1,7 @@
 import assertTreeIsDefined from "../common/assertTreeIsDefined.js";
 
-const fnPromiseMap = new WeakMap();
+const fnPromiseMap = new Map();
+const codePromiseMap = new Map();
 
 /**
  * Evaluate the given function only once and cache the result.
@@ -11,8 +12,23 @@ const fnPromiseMap = new WeakMap();
  */
 export default async function once(fn) {
   assertTreeIsDefined(this, "origami:once");
+
+  const code = /** @type {any} */ (fn).code;
+  if (code) {
+    // Origami function, cache by code
+    if (!codePromiseMap.has(code)) {
+      // Don't wait for promise to resolve
+      const promise = fn.call(this);
+      codePromiseMap.set(code, promise);
+    }
+    return codePromiseMap.get(code);
+  }
+
+  // Regular function, cache by function
   if (!fnPromiseMap.has(fn)) {
-    fnPromiseMap.set(fn, fn.call(this));
+    // Don't wait for promise to resolve
+    const promise = fn.call(this);
+    fnPromiseMap.set(fn, promise);
   }
   return fnPromiseMap.get(fn);
 }
--- origami/test/origami/once.test.js
+++ origami/test/origami/once.test.js
@@ -1,12 +1,31 @@
+import { ObjectTree } from "@weborigami/async-tree";
+import { evaluate, ops } from "@weborigami/language";
 import assert from "node:assert";
 import { describe, test } from "node:test";
 import once from "../../src/origami/once.js";
 
 describe("once", () => {
-  test("evaluates a function only once", async () => {
+  test("evaluates a generic function only once", async () => {
     let counter = 0;
     const promise = once.call(null, () => ++counter);
     assert.strictEqual(await promise, 1);
     assert.strictEqual(await promise, 1);
+  });
+
+  test("evaluates an Origami lambda with given code only once", async () => {
+    let counter = 0;
+    const scope = new ObjectTree({
+      increment: () => ++counter,
+    });
+    // Create two lambdas with the same code
+    const code = [ops.lambda, [], [[ops.scope, "increment"]]];
+    // @ts-ignore
+    const lambda1 = await evaluate.call(scope, code);
+    const result1 = await once.call(null, lambda1);
+    assert.strictEqual(result1, 1);
+    // @ts-ignore
+    const lambda2 = await evaluate.call(scope, code);
+    const result2 = await once.call(null, lambda2);
+    assert.strictEqual(result2, 1);
   });
 });
