247516d07fc0cb3f0fc3a601e3dba8e586d9cc74
Jan Miksovsky <jan@miksovsky.com>
2025-01-19 11:47:23 -0800

    Expose package: protocol as a builtin

--- origami/main.js
+++ origami/main.js
@@ -7,6 +7,7 @@ export * from "./src/handlers/handlers.js";
 export * from "./src/image/image.js";
 export { builtinsTree } from "./src/internal.js";
 export * from "./src/origami/origami.js";
+export { default as packageBuiltin } from "./src/protocols/package.js";
 export * from "./src/server/server.js";
 export * from "./src/site/site.js";
 export * from "./src/text/text.js";
