fb76ca921250604878c7d6ced2473c2f672966bb
Jan Miksovsky <jan@miksovsky.com>
2025-01-25 11:34:15 -0800

    Tracing

--- language/src/runtime/tracing.js
+++ language/src/runtime/tracing.js
@@ -1,4 +1,5 @@
 import { box, isStringLike, scope, toString } from "@weborigami/async-tree";
+import { isPrimitive } from "@weborigami/async-tree/src/utilities.js";
 import codeFragment from "./codeFragment.js";
 import {
   codeSymbol,
@@ -7,51 +8,59 @@ import {
   traceSymbol,
 } from "./symbols.js";
 
-export default function tracing(context, code, evaluated, result) {
-  if (typeof result === "symbol") {
+export default function tracing(context, code, evaluation, result) {
+  if (result == null || typeof result === "symbol") {
     return result;
   }
 
-  if (result) {
-    result = box(result);
-    try {
-      if (result[sourceSymbol] === undefined) {
-        Object.defineProperty(result, sourceSymbol, {
-          get() {
-            return code.location ? codeFragment(code.location) : null;
+  let call = null;
+  if (result[traceSymbol]) {
+    // Function call
+    call = result[traceSymbol];
+  }
+
+  result = box(result);
+  try {
+    Object.defineProperty(result, codeSymbol, {
+      configurable: true,
+      value: code,
+      enumerable: false,
+    });
+    Object.defineProperty(result, scopeSymbol, {
+      configurable: true,
+      get() {
+        return scope(context).trees;
+      },
+      enumerable: false,
+    });
+    Object.defineProperty(result, sourceSymbol, {
+      configurable: true,
+      get() {
+        return code.location ? codeFragment(code.location) : null;
+      },
+      enumerable: false,
+    });
+    Object.defineProperty(result, traceSymbol, {
+      configurable: true,
+      get() {
+        const source = code.location ? codeFragment(code.location) : null;
+        const url = code.location?.source?.url;
+        return Object.assign(
+          {
+            value: result.valueOf?.() ?? result,
+            source,
+            url,
+            evaluation: evaluation.map(format),
           },
-          enumerable: false,
-        });
-      }
-      if (result[codeSymbol] === undefined) {
-        Object.defineProperty(result, codeSymbol, {
-          value: code,
-          enumerable: false,
-        });
-      }
-      if (result[scopeSymbol] === undefined) {
-        Object.defineProperty(result, scopeSymbol, {
-          get() {
-            return scope(context).trees;
-          },
-          enumerable: false,
-        });
-      }
-      if (result[traceSymbol] === undefined) {
-        Object.defineProperty(result, traceSymbol, {
-          get() {
-            return {
-              value: result.valueOf?.() ?? result,
-              source: code.location ? codeFragment(code.location) : null,
-              evaluated: evaluated.map(format),
-            };
-          },
-          enumerable: false,
-        });
-      }
-    } catch (/** @type {any} */ error) {
-      // Ignore errors.
-    }
+          call && {
+            call,
+          }
+        );
+      },
+      enumerable: false,
+    });
+  } catch (/** @type {any} */ error) {
+    // Ignore errors.
   }
 
   return result;
@@ -60,11 +69,14 @@ export default function tracing(context, code, evaluated, result) {
 function format(object) {
   if (object[traceSymbol]) {
     return object[traceSymbol];
+  } else if (isPrimitive(object)) {
+    return object;
   } else if (object instanceof Number || object instanceof String) {
     return object.valueOf();
   } else if (isStringLike(object)) {
     return toString(object);
   } else if (object instanceof Function) {
+    // Ops have a toString method for a friendly name
     return Object.getOwnPropertyNames(object).includes("toString")
       ? object.toString()
       : object.name;
