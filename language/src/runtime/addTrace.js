7aaa5db48872f76dc0d95082e9dc4c30b2883d7c
Jan Miksovsky <jan@miksovsky.com>
2025-02-05 11:04:59 -0800

    Define trace structure

--- language/src/runtime/addTrace.js
+++ language/src/runtime/addTrace.js
@@ -1,8 +1,13 @@
-import { box, scope } from "@weborigami/async-tree";
+import { box, isStringLike, scope, toString } from "@weborigami/async-tree";
 import codeFragment from "./codeFragment.js";
-import { codeSymbol, scopeSymbol, sourceSymbol } from "./symbols.js";
+import {
+  codeSymbol,
+  scopeSymbol,
+  sourceSymbol,
+  traceSymbol,
+} from "./symbols.js";
 
-export default function tracing(context, code, evaluated, result) {
+export default function addTrace(context, code, intermediates, result) {
   if (typeof result === "symbol") {
     return result;
   }
@@ -10,7 +15,7 @@ export default function tracing(context, code, evaluated, result) {
   if (result) {
     result = box(result);
     try {
-      if (result[sourceSymbol] === undefined) {
+      if (!(sourceSymbol in result)) {
         Object.defineProperty(result, sourceSymbol, {
           get() {
             return code.location ? codeFragment(code.location) : null;
@@ -18,13 +23,13 @@ export default function tracing(context, code, evaluated, result) {
           enumerable: false,
         });
       }
-      if (result[codeSymbol] === undefined) {
+      if (!(codeSymbol in result)) {
         Object.defineProperty(result, codeSymbol, {
           value: code,
           enumerable: false,
         });
       }
-      if (result[scopeSymbol] === undefined) {
+      if (!(scopeSymbol in result)) {
         Object.defineProperty(result, scopeSymbol, {
           get() {
             return scope(context).trees;
@@ -32,18 +37,18 @@ export default function tracing(context, code, evaluated, result) {
           enumerable: false,
         });
       }
-      // if (result[traceSymbol] === undefined) {
-      //   Object.defineProperty(result, traceSymbol, {
-      //     get() {
-      //       return {
-      //         value: result.valueOf?.() ?? result,
-      //         source: code.location ? codeFragment(code.location) : null,
-      //         evaluated: evaluated.map(format),
-      //       };
-      //     },
-      //     enumerable: false,
-      //   });
-      // }
+      if (!(traceSymbol in result)) {
+        Object.defineProperty(result, traceSymbol, {
+          get() {
+            return createTrace(
+              result,
+              code.location.source.text,
+              intermediates
+            );
+          },
+          enumerable: false,
+        });
+      }
     } catch (/** @type {any} */ error) {
       // Ignore errors.
     }
@@ -52,18 +57,36 @@ export default function tracing(context, code, evaluated, result) {
   return result;
 }
 
-// function format(object) {
-//   if (object[traceSymbol]) {
-//     return object[traceSymbol];
-//   } else if (object instanceof Number || object instanceof String) {
-//     return object.valueOf();
-//   } else if (isStringLike(object)) {
-//     return toString(object);
-//   } else if (object instanceof Function) {
-//     return Object.getOwnPropertyNames(object).includes("toString")
-//       ? object.toString()
-//       : object.name;
-//   } else {
-//     return object;
-//   }
-// }
+function createTrace(result, source, intermediateValues) {
+  const value = format(result);
+  let intermediateTraces;
+  if (intermediateValues) {
+    intermediateValues.shift();
+    intermediateTraces = intermediateValues.map((intermediate) => {
+      return intermediate[traceSymbol] || createTrace(intermediate, source);
+    });
+  }
+  return Object.assign(
+    {
+      value,
+      // source,
+    },
+    intermediateTraces && { intermediates: intermediateTraces }
+  );
+}
+
+function format(object) {
+  if (typeof object === "number") {
+    return object;
+  } else if (object instanceof Number || object instanceof String) {
+    return object.valueOf();
+  } else if (isStringLike(object)) {
+    return toString(object);
+  } else if (object instanceof Function) {
+    return Object.getOwnPropertyNames(object).includes("toString")
+      ? object.toString()
+      : object.name;
+  } else {
+    return object;
+  }
+}
---
+++ language/test/runtime/addTrace.test.js
@@ -0,0 +1,45 @@
+import assert from "node:assert";
+import { describe, test } from "node:test";
+import * as compile from "../../src/compiler/compile.js";
+import { traceSymbol } from "../../src/runtime/symbols.js";
+
+describe("addTrace", () => {
+  test("adds trace structure to results", async () => {
+    const source = `
+      {
+        f: (x) => x + 1
+        g: 2 * f/(3)
+      }
+    `;
+    const program = compile.expression(source);
+    const object = await program.call(null);
+    const result = await object.g;
+    assert.strictEqual(result.valueOf(), 8);
+    const trace = result[traceSymbol];
+    assert.deepStrictEqual(trace, {
+      value: 8,
+      // source,
+      intermediates: [
+        {
+          value: 2,
+          // source
+        },
+        {
+          value: 4,
+          // source,
+          intermediates: [
+            {
+              value: 3,
+              // source,
+              intermediates: [{ value: "x" }],
+            },
+            {
+              value: 1,
+              // source
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
