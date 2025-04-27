f51cd4a566e7208c04f0dfb3653e9f60e234e7ff
Jan Miksovsky <jan@miksovsky.com>
2025-04-26 20:36:54 -0400

    Use CSSOM parser (same one as in jsdom)

--- origami/src/site/crawler/pathsInCss.js
+++ origami/src/site/crawler/pathsInCss.js
@@ -1,19 +1,44 @@
+import CSSOM from "rrweb-cssom";
 import { isCrawlableHref, normalizeHref } from "./utilities.js";
 
 export default function findPathsInCss(css) {
   const crawlablePaths = [];
   const resourcePaths = [];
-  let match;
 
-  // Find `url()` functions.
-  const urlRegex = /url\(["']?(?<href>[^"')]*?)["']?\)/g;
-  while ((match = urlRegex.exec(css))) {
-    const href = normalizeHref(match.groups?.href);
-    if (href) {
+  const cssom = new CSSOM.parse(css);
+  const rules = cssom.cssRules;
+  for (const rule of rules) {
+    if (rule instanceof CSSOM.CSSFontFaceRule) {
+      // @font-face
+      const src = rule.style.getPropertyValue("src");
+      const href = findUrl(src);
       if (isCrawlableHref(href)) {
         crawlablePaths.push(href);
       } else {
         resourcePaths.push(href);
+      }
+    } else if (rule instanceof CSSOM.CSSImportRule) {
+      // @import
+      const href = normalizeHref(rule.href);
+      if (href) {
+        if (isCrawlableHref(href)) {
+          crawlablePaths.push(href);
+        } else {
+          resourcePaths.push(href);
+        }
+      }
+    } else if (rule instanceof CSSOM.CSSStyleRule) {
+      // Regular rule, search for `url()` functions
+      const style = rule.style;
+      for (let i = 0; i < style.length; i++) {
+        const name = style[i];
+        const value = style.getPropertyValue(name);
+        const href = findUrl(value);
+        if (isCrawlableHref(href)) {
+          crawlablePaths.push(href);
+        } else {
+          resourcePaths.push(href);
+        }
       }
     }
   }
@@ -22,4 +47,12 @@ export default function findPathsInCss(css) {
     crawlablePaths,
     resourcePaths,
   };
+}
+
+// Search for `url()` functions in a CSS value
+function findUrl(propertyValue) {
+  const urlRegex = /url\(["']?(?<href>[^"')]*?)["']?\)/g;
+  const match = urlRegex.exec(propertyValue);
+  const href = match?.groups?.href;
+  return href ? normalizeHref(href) : null;
 }
--- origami/test/site/crawler/pathsInCss.test.js
+++ origami/test/site/crawler/pathsInCss.test.js
@@ -3,18 +3,29 @@ import { describe, test } from "node:test";
 import pathsInCss from "../../../src/site/crawler/pathsInCss.js";
 
 describe("pathsInCss", () => {
-  test("finds paths in HTML", async () => {
+  test("finds paths in CSS", async () => {
     const css = `
-      @import url("styles.css");
+      /* url("bogus.css") ignored because it's in a comment */
+      
+      @import "stringImport.css" screen and (min-width: 600px);
+      @import url("import.css");
 
+      @font-face {
+        font-family: "OpenSans";
+        src:
+          local("Open Sans"),
+          url("/fonts/OpenSans.woff2") format("woff2"),
+          url("/fonts/OpenSans.woff")  format("woff");
+      }
+      
       body {
         background-image: url("background.jpg");
       }
     `;
     const paths = await pathsInCss(css);
     assert.deepEqual(paths, {
-      crawlablePaths: ["styles.css"],
-      resourcePaths: ["background.jpg"],
+      crawlablePaths: ["stringImport.css", "import.css"],
+      resourcePaths: ["/fonts/OpenSans.woff2", "background.jpg"],
     });
   });
 });
