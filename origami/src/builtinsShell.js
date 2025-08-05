1c19a3696b7d43b443f8624ac5093e4ca7990a58
Jan Miksovsky <jan@miksovsky.com>
2025-08-05 15:57:00 -0700

    Shell builtins for Tree and Origami mirror the corresponding builtins for programs

--- origami/src/builtinsShell.js
+++ origami/src/builtinsShell.js
@@ -1,12 +1,32 @@
+import { text as treeText } from "@weborigami/async-tree";
 import builtinsJse from "./builtinsJse.js";
 import * as dev from "./dev/dev.js";
+import * as image from "./image/image.js";
 import * as origami from "./origami/origami.js";
+import files from "./protocols/files.js";
+import * as site from "./site/site.js";
+import * as text from "./text/text.js";
 import * as tree from "./tree/tree.js";
 
 let builtins;
 
 export default function builtinsShell() {
   if (!builtins) {
+    const Tree = {
+      ...tree,
+      indent: text.indent,
+      json: origami.json,
+      text: treeText,
+    };
+
+    const Origami = {
+      files,
+      image,
+      ...origami,
+      ...site,
+      ...text,
+    };
+
     builtins = {
       // All JSE builtins
       ...builtinsJse(),
@@ -15,8 +35,8 @@ export default function builtinsShell() {
       ...dev,
 
       Dev: dev,
-      Origami: origami,
-      Tree: tree,
+      Origami,
+      Tree,
     };
   }
 
