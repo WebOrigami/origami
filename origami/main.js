9388bd53d8fc6141eceb5aad5377b60557a7de4b
Jan Miksovsky <jan@miksovsky.com>
2025-08-26 10:25:47 -0700

    Clean up exports

--- origami/main.js
+++ origami/main.js
@@ -3,13 +3,7 @@ export { toString } from "./src/common/utilities.js";
 export * from "./src/dev/dev.js";
 export { default as handlerBuiltins } from "./src/handlers/handlerBuiltins.js";
 export * from "./src/handlers/handlers.js";
-export * as image from "./src/origami/image/image.js";
 export * from "./src/origami/origami.js";
 export { default as origamiHighlightDefinition } from "./src/origami/origamiHighlightDefinition.js";
-export { default as packageBuiltin } from "./src/protocols/package.js";
+export { default as package } from "./src/protocols/package.js";
 export * from "./src/server/server.js";
-export * from "./src/site/site.js";
-export * from "./src/text/text.js";
-
-// TODO: Remove once these all moves to async-tree
-export * from "./src/tree/tree.js";
