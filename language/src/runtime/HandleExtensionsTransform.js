b103f4c9c6302d3999772644c0f0cd5a846b2400
Jan Miksovsky <jan@miksovsky.com>
2025-10-27 15:38:52 -0700

    HandlExtensionsTransform takes care to keep the extended class read-only or read/write just like the input Base class

--- language/src/runtime/HandleExtensionsTransform.js
+++ language/src/runtime/HandleExtensionsTransform.js
@@ -1,19 +1,41 @@
 import handleExtension from "./handleExtension.js";
 
 /**
- * @typedef {import("@weborigami/types").AsyncTree} AsyncTree
- * @typedef {import("../../index.ts").Constructor<AsyncTree>} AsyncTreeConstructor
+ * @typedef {import("../../index.ts").Constructor<Map>} MapConstructor
  * @typedef {import("@weborigami/async-tree").UnpackFunction} FileUnpackFunction
  *
- * @param {AsyncTreeConstructor} Base
+ * @param {MapConstructor} Base
  */
 export default function HandleExtensionsTransform(Base) {
-  return class HandleExtensions extends Base {
+  class HandleExtensions extends Base {
+    // Implement delete (and set) to keep the Map read-write
+    delete(key) {
+      return super.delete(key);
+    }
+
     get(key) {
       const value = super.get(key);
       return value instanceof Promise
         ? value.then((resolved) => handleExtension(resolved, key, this))
         : handleExtension(value, key, this);
     }
-  };
+
+    // See delete()
+    set(key, value) {
+      return super.set(key, value);
+    }
+  }
+
+  if (Base.prototype.readOnly) {
+    // Remove delete and set methods to keep the Map read-only. The base delete
+    // and set methods will exist (because it's a Map) but for our purposes the
+    // class is read-only.
+
+    // @ts-ignore
+    delete HandleExtensions.prototype.delete;
+    // @ts-ignore
+    delete HandleExtensions.prototype.set;
+  }
+
+  return HandleExtensions;
 }
