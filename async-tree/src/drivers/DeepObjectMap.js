76723bb981d6ef221d385bad739539674e6e68bb
Jan Miksovsky <jan@miksovsky.com>
2025-10-27 16:06:44 -0700

    DeepObjectMap is read/write

--- async-tree/src/drivers/DeepObjectMap.js
+++ async-tree/src/drivers/DeepObjectMap.js
@@ -3,6 +3,11 @@ import isPlainObject from "../utilities/isPlainObject.js";
 import ObjectMap from "./ObjectMap.js";
 
 export default class DeepObjectMap extends ObjectMap {
+  // Implement delete (and set) to keep the Map read-write
+  delete(key) {
+    return super.delete(key);
+  }
+
   get(key) {
     let value = super.get(key);
     if (value instanceof Array || isPlainObject(value)) {
@@ -13,5 +18,10 @@ export default class DeepObjectMap extends ObjectMap {
 
   isSubtree(value) {
     return value instanceof Array || isPlainObject(value) || isAsyncTree(value);
+  }
+
+  // See delete()
+  set(key, value) {
+    return super.set(key, value);
   }
 }
