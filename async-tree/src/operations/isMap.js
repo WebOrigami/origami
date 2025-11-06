2c04a04917b15cd85b43b866a0c3ca10018e08c1
Jan Miksovsky <jan@miksovsky.com>
2025-11-06 12:15:30 -0800

    Generalize detection of Map-like interface

--- async-tree/src/operations/isMap.js
+++ async-tree/src/operations/isMap.js
@@ -1,7 +1,8 @@
 import AsyncMap from "../drivers/AsyncMap.js";
 
 /**
- * Return true if the indicated object is a sync or async map.
+ * Return true if the indicated object is a Map instance or supports
+ * a `Map` compatible interface.
  *
  * @typedef {import("../../index.ts").SyncOrAsyncMap} SyncOrAsyncMap
  *
@@ -9,5 +10,29 @@ import AsyncMap from "../drivers/AsyncMap.js";
  * @returns {object is Map|AsyncMap}
  */
 export default function isMap(object) {
-  return object instanceof Map || object instanceof AsyncMap;
+  if (object instanceof Map || object instanceof AsyncMap) {
+    // Known positive cases
+    return true;
+  }
+
+  // Check for Map-like interface
+  if (
+    object.clear instanceof Function &&
+    object.delete instanceof Function &&
+    object.entries instanceof Function &&
+    object.forEach instanceof Function &&
+    object.get instanceof Function &&
+    object.has instanceof Function &&
+    object.keys instanceof Function &&
+    object.set instanceof Function &&
+    object.values instanceof Function &&
+    object.constructor.groupBy instanceof Function &&
+    "size" in object &&
+    (object[Symbol.iterator] instanceof Function ||
+      object[Symbol.asyncIterator] instanceof Function)
+  ) {
+    return true;
+  }
+
+  return false;
 }
