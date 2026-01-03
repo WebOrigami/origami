51b2c912966b2cf46b6d51a6d18c6149aebc05ab
Jan Miksovsky <jan@miksovsky.com>
2026-01-02 20:26:35 -0800

    Expose initializeBuiltins

--- origami/main.js
+++ origami/main.js
@@ -1,5 +1,6 @@
 export { default as documentObject } from "./src/common/documentObject.js";
 export * as Dev from "./src/dev/dev.js";
+export { default as initializeBuiltins } from "./src/initializeBuiltins.js";
 export * as Origami from "./src/origami/origami.js";
 export { default as origamiHighlightDefinition } from "./src/origami/origamiHighlightDefinition.js";
 export { default as constructResponse } from "./src/server/constructResponse.js";
