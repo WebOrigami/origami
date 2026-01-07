ba4eaa2c754a2e5b11ce748a265c2cbfeda19464
Jan Miksovsky <jan@miksovsky.com>
2026-01-07 12:01:31 -0800

    projectGlobals can accept a directory in a project

--- language/src/project/projectGlobals.js
+++ language/src/project/projectGlobals.js
@@ -4,14 +4,14 @@ import projectConfig from "./projectConfig.js";
 let globals;
 
 // Core globals plus project config
-export default async function projectGlobals() {
+export default async function projectGlobals(dir = process.cwd()) {
   if (!globals) {
     // Start with core globals
     globals = await coreGlobals();
     // Now get config. The config.ori file may require access to globals,
     // which will obtain the core globals set above. Once we've got the
     // config, we add it to the globals.
-    const config = await projectConfig();
+    const config = await projectConfig(dir);
     Object.assign(globals, config);
   }
 
