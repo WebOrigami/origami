20dd1ba83c328879f521ddf616935c21dc66ed88
Jan Miksovsky <jan@miksovsky.com>
2026-01-15 11:43:05 -0800

    Export serialization helpers

--- origami/main.js
+++ origami/main.js
@@ -1,4 +1,5 @@
 export { default as documentObject } from "./src/common/documentObject.js";
+export * from "./src/common/serialize.js";
 export * as Dev from "./src/dev/dev.js";
 export { default as initializeBuiltins } from "./src/initializeBuiltins.js";
 export * as Origami from "./src/origami/origami.js";
