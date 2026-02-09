550121c23c206a4be88a6c8394fb81621f59bc75
Jan Miksovsky <jan@miksovsky.com>
2026-02-09 11:20:57 -0800

    Restore ability of Origami.document to accept packed input or plain object input

--- origami/src/origami/document.js
+++ origami/src/origami/document.js
@@ -1,13 +1,20 @@
-import { args } from "@weborigami/async-tree";
+import { args, isPlainObject, isUnpackable } from "@weborigami/async-tree";
 import documentObject from "../common/documentObject.js";
 
 /**
  * @typedef {import("@weborigami/async-tree").Stringlike} Stringlike
+ * @typedef {import("@weborigami/async-tree").PlainObject} PlainObject
  *
- * @param {Stringlike} text
+ * @param {Stringlike|PlainObject} input
  * @param {any} [data]
  */
-export default async function documentBuiltin(text, data) {
-  text = args.stringlike(text, "Origami.document");
-  return documentObject(text, data);
+export default async function documentBuiltin(input, data) {
+  if (isUnpackable(input)) {
+    // Unpack the input first, might already be a document object.
+    input = await input.unpack();
+  }
+  input = isPlainObject(input)
+    ? input
+    : args.stringlike(input, "Origami.document");
+  return documentObject(input, data);
 }
