4ce819b140768ee16fe300bacd349fda6124083a
Jan Miksovsky <jan@miksovsky.com>
2026-03-19 09:44:18 -0700

    When wrapping a `Set` as a `Map`, treat it like an array with integer keys

--- async-tree/src/drivers/SetMap.js
+++ async-tree/src/drivers/SetMap.js
@@ -1,13 +1,16 @@
 import SyncMap from "./SyncMap.js";
 
 /**
- * A map of Set objects.
+ * A standard `Set` represented as a `Map`. The keys are integer indices; the
+ * values are the set's values.
  */
 export default class SetMap extends SyncMap {
   /**
    * @param {Set} set
    */
   constructor(set) {
-    super(set.entries());
+    // Treat the set as an array-like map
+    const entries = Array.from(set).map((value, index) => [index, value]);
+    super(entries);
   }
 }
--- async-tree/test/drivers/SetMap.test.js
+++ async-tree/test/drivers/SetMap.test.js
@@ -6,20 +6,20 @@ describe("SetMap", () => {
   test("can get the keys of the map", async () => {
     const set = new Set(["a", "b", "c"]);
     const fixture = new SetMap(set);
-    assert.deepEqual(Array.from(await fixture.keys()), ["a", "b", "c"]);
+    assert.deepEqual(Array.from(await fixture.keys()), [0, 1, 2]);
   });
 
   test("can get the value for a key", async () => {
     const set = new Set(["a", "b", "c"]);
     const fixture = new SetMap(set);
-    const a = await fixture.get("a");
+    const a = await fixture.get(0);
     assert.equal(a, "a");
   });
 
   test("getting an unsupported key returns undefined", async () => {
     const set = new Set(["a", "b", "c"]);
     const fixture = new SetMap(set);
-    assert.equal(await fixture.get("d"), undefined);
+    assert.equal(await fixture.get(3), undefined);
   });
 
   test("getting a null/undefined key throws an exception", async () => {
