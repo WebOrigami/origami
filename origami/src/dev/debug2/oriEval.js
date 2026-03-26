de3ec2a0393fca65479e078c7bdf14fb614b99e5
Jan Miksovsky <jan@miksovsky.com>
2026-03-26 15:39:53 -0700

    Allow debugger to force reevaluation of an expression by incrementing a counter

--- origami/src/dev/debug2/oriEval.js
+++ origami/src/dev/debug2/oriEval.js
@@ -1,22 +1,37 @@
-import ori from "../../origami/ori.js";
+import { trailingSlash } from "@weborigami/async-tree";
+import { evaluate } from "@weborigami/language";
 
-let mapParentToResults = new WeakMap();
+const mapParentToResult = new WeakMap();
 
+/**
+ * Return a route that evaluates a key containing an expression.
+ *
+ * Because this route may be used multiple times to return the resources for a
+ * page, we cache the result for each parent and key.
+ *
+ * To allow multiple evaluations of the same expression, we expect the key to be
+ * of the form `<counter>,<expression>`, where the counter is a number that will
+ * be used for caching but ignored for evaluation. The debugger increments the
+ * counter to force reevaluation of the same expression.
+ */
 export default async function oriEval(parent) {
-  return async (encodedExpression) => {
-    const expression = decodeURIComponent(encodedExpression);
-
-    let lastResult = mapParentToResults.get(parent);
-    if (lastResult) {
-      const { expression, value } = lastResult;
-      if (expression === encodedExpression) {
-        return value;
-      }
+  return async (key) => {
+    const normalizedKey = trailingSlash.remove(key);
+    let result = mapParentToResult.get(parent);
+    if (result?.key === normalizedKey) {
+      return result.value;
     }
 
-    const value = await ori(expression, { parent });
-    const result = { expression, value };
-    mapParentToResults.set(parent, result);
+    const regex = /^\d+,(?<expression>.*)$/;
+    const match = normalizedKey.match(regex);
+    let value;
+    if (match) {
+      const expression = decodeURIComponent(match.groups.expression);
+      value = await evaluate(expression, { parent });
+    }
+
+    mapParentToResult.set(parent, { key: normalizedKey, value });
+
     return value;
   };
 }
