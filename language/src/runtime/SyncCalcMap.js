d3635f8345a31b28c0a94814c3e3f9ba7fa1dc78
Jan Miksovsky <jan@miksovsky.com>
2026-04-21 11:44:27 -0700

    Fold InvokeFunctionsTransform into SyncCalcMap to match AsyncCalcMap

--- language/src/runtime/SyncCalcMap.js
+++ language/src/runtime/SyncCalcMap.js
@@ -1,7 +1,25 @@
-import { SyncMap } from "@weborigami/async-tree";
-import InvokeFunctionsTransform from "./InvokeFunctionsTransform.js";
+import { isPlainObject, SyncMap } from "@weborigami/async-tree";
 import SyncCacheTransform from "./SyncCacheTransform.js";
 
 export default class SyncCalcMap extends SyncCacheTransform(
   InvokeFunctionsTransform(SyncMap),
 ) {}
+
+function InvokeFunctionsTransform(Base) {
+  return class extends Base {
+    constructor(iterable) {
+      if (isPlainObject(iterable)) {
+        iterable = Object.entries(iterable);
+      }
+      super(iterable);
+    }
+
+    get(key) {
+      let value = super.get(key);
+      if (typeof value === "function") {
+        value = value();
+      }
+      return value;
+    }
+  };
+}
