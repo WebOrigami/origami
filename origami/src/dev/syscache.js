28f0f8f4d0c1848c242003f97eb35db6f66c222a
Jan Miksovsky <jan@miksovsky.com>
2026-04-24 17:29:54 -0700

    syscache command serves YAML that includes trailing slashes in keys

--- origami/src/dev/syscache.js
+++ origami/src/dev/syscache.js
@@ -1,4 +1,10 @@
 import { systemCache } from "@weborigami/language";
+import * as YAMLModule from "yaml";
+
+// The "yaml" package doesn't seem to provide a default export that the browser can
+// recognize, so we have to handle two ways to accommodate Node and the browser.
+// @ts-ignore
+const YAML = YAMLModule.default ?? YAMLModule.YAML;
 
 export default function syscache() {
   /** @type {any} */
@@ -16,5 +22,16 @@ export default function syscache() {
   // Sort the entries by key
   entries.sort(([keyA], [keyB]) => keyA.localeCompare(keyB));
 
-  return new Map(entries);
+  const result = new Map(entries);
+
+  // When served, render as YAML and preserve trailing slashes
+  Object.defineProperty(result, "pack", {
+    configurable: true,
+    enumerable: false,
+    get() {
+      return () => YAML.stringify(result);
+    },
+  });
+
+  return result;
 }
