a99a129c45baf83152ffab6d6391c95863d94aa9
Jan Miksovsky <jan@miksovsky.com>
2026-05-25 13:40:22 -0700

    Tree.shuffle() accepts `randoms` function argument

--- async-tree/src/operations/shuffle.js
+++ async-tree/src/operations/shuffle.js
@@ -3,16 +3,22 @@ import * as args from "../utilities/args.js";
 import keys from "./keys.js";
 
 /**
- * Return a new tree with the original's keys shuffled
+ * Return a new tree with the original's keys shuffled.
+ *
+ * The `randoms` option allows you to provide a function that either returns a
+ * random number between 0 and 1 (like `Math.random`) or a random integer. This
+ * can be used to create deterministic shuffling.
  *
  * @typedef {import("../../index.ts").Maplike} Maplike
+ * @typedef {import("../../index.ts").Stringlike} Stringlike
  *
  * @param {Maplike} maplike
- * @param {boolean?} reshuffle
+ * @param {{ randoms?: (() => number) }?} options
  * @returns {Promise<AsyncMap>}
  */
-export default async function shuffle(maplike, reshuffle = false) {
+export default async function shuffle(maplike, options = {}) {
   const source = await args.map(maplike, "Tree.shuffle");
+  const randoms = options?.randoms ?? Math.random;
 
   let mapKeys;
 
@@ -24,9 +30,9 @@ export default async function shuffle(maplike, reshuffle = false) {
     },
 
     async *keys() {
-      if (!mapKeys || reshuffle) {
+      if (!mapKeys) {
         mapKeys = await keys(source);
-        shuffleArray(mapKeys);
+        shuffleArray(mapKeys, randoms);
       }
       yield* mapKeys;
     },
@@ -42,10 +48,17 @@ export default async function shuffle(maplike, reshuffle = false) {
  *
  * Performs a Fisher-Yates shuffle. From http://sedition.com/perl/javascript-fy.html
  */
-export function shuffleArray(array) {
+export function shuffleArray(array, randoms) {
   let i = array.length;
   while (--i >= 0) {
-    const j = Math.floor(Math.random() * (i + 1));
+    const random = randoms();
+
+    const j =
+      random < 1
+        ? // Like Math.random
+          Math.floor(random * (i + 1))
+        : // Random number
+          Math.floor(random) % (i + 1);
     const temp = array[i];
     array[i] = array[j];
     array[j] = temp;
