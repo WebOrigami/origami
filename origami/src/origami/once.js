a5ec15bc1e33abd35c7082ae91cc29f99360ce68
Jan Miksovsky <jan@miksovsky.com>
2026-06-09 11:23:41 -0700

    Mark Origami.once for deprecation

--- origami/src/origami/once.js
+++ origami/src/origami/once.js
@@ -1,4 +1,4 @@
-import { args } from "@weborigami/async-tree";
+import { args, interop } from "@weborigami/async-tree";
 
 const fnPromiseMap = new Map();
 const codePromiseMap = new Map();
@@ -9,6 +9,9 @@ const codePromiseMap = new Map();
  * @param {Function} fn
  */
 export default async function once(fn) {
+  interop.warn(
+    `Now that Origami caches most results, Origami.once is no longer needed and will be removed in a future release. If you believe you have a need for it, please mention your use case in the Origami chat.`,
+  );
   fn = args.fn(fn, "Origami.once");
   const code = /** @type {any} */ (fn).code;
   if (code) {
