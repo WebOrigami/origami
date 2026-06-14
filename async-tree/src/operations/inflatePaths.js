92783bbaee451bfc233cd5d65d94d455b64f6daf
Jan Miksovsky <jan@miksovsky.com>
2026-06-14 08:52:58 -0700

    inflatePaths() accepts an optional classFn that will be instantiated to create new nodes

--- async-tree/src/operations/inflatePaths.js
+++ async-tree/src/operations/inflatePaths.js
@@ -5,23 +5,24 @@ import keysFromPath from "../utilities/keysFromPath.js";
 /**
  * Given a mapping of string paths to values, return the described tree.
  */
-export default async function inflatePaths(maplike) {
-  const map = await args.map(maplike, "Tree.inflatePaths", { deep: true });
+export default async function inflatePaths(maplike, options = {}) {
+  const map = await args.map(maplike, "Tree.inflatePaths");
 
-  const result = new SyncMap();
+  const classFn = options.classFn ?? SyncMap;
+  const result = new classFn();
   for await (const [path, value] of map) {
     const keys = keysFromPath(path);
-    setValue(result, keys, value);
+    setValue(result, keys, value, classFn);
   }
   return result;
 }
 
 // Add the value to the tree at the given path of keys
-function setValue(map, keys, value) {
+function setValue(map, keys, value, classFn) {
   let node = map;
   for (const key of keys.slice(0, -1)) {
     // Create a new node if one doesn't exist yet
-    node = node.getOrInsertComputed(key, () => new SyncMap());
+    node = node.getOrInsertComputed(key, () => new classFn());
   }
   node.set(keys[keys.length - 1], value);
 }
