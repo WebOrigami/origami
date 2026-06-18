60a42a81294a936cc2dd6c46e9087f0fc23a9f14
Jan Miksovsky <jan@miksovsky.com>
2026-06-18 14:38:26 -0700

    Type fixes

--- language/src/runtime/AsyncCacheTransform.d.ts
+++ language/src/runtime/AsyncCacheTransform.d.ts
@@ -1,5 +1,9 @@
 import { Mixin } from "../../index.ts";
 
-declare const AsyncCacheTransform: Mixin<{}>
+declare const AsyncCacheTransform: Mixin<{
+  cachePathForKey(key: string): string;
+  onKeysChange(key: string): void;
+  onValueChange(key: string): void;
+}>
 
 export default AsyncCacheTransform;
--- language/src/runtime/SyncCacheTransform.d.ts
+++ language/src/runtime/SyncCacheTransform.d.ts
@@ -1,8 +1,9 @@
 import { Mixin } from "../../index.ts";
 
 declare const SyncCacheTransform: Mixin<{
-  get cachePath(): string;
   cachePathForKey(key: string): string;
+  onKeysChange(key: string): void;
+  onValueChange(key: string): void;
 }>
 
 export default SyncCacheTransform;
