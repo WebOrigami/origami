9dceaf8836b57762d772e0b6f297cd7a41ad0108
Jan Miksovsky <jan@miksovsky.com>
2026-09-04 15:51:50 -0700

    forEach supports `deep` option

--- async-tree/src/operations/forEach.js
+++ async-tree/src/operations/forEach.js
@@ -1,5 +1,5 @@
 import * as args from "../utilities/args.js";
-
+import isMap from "./isMap.js";
 /**
  * Calls callbackFn once for each key-value pair present in the map.
  *
@@ -7,15 +7,20 @@ import * as args from "../utilities/args.js";
  *
  * @param {Maplike} maplike
  * @param {Function} callbackFn
+ * @param {{ deep: boolean }} [options]
  */
-export default async function forEach(maplike, callbackFn) {
-  const map = await args.map(maplike, "Tree.forEach");
+export default async function forEach(maplike, callbackFn, options) {
+  const map = await args.map(maplike, "Tree.forEach", options);
   if (typeof callbackFn !== "function") {
     throw new TypeError("Tree.forEach: Expected a function argument.");
   }
 
   for await (const key of map.keys()) {
     const value = await map.get(key);
-    await callbackFn(value, key, map);
+    if (isMap(value) && options?.deep) {
+      await forEach(value, callbackFn, options);
+    } else {
+      await callbackFn(value, key, map);
+    }
   }
 }
