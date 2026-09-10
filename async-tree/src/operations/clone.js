ae265c9c3aa7ac7e4c3444b8ad4e672b85ca9bdb
Jan Miksovsky <jan@miksovsky.com>
2026-09-10 15:38:30 -0700

    Simplify clone() to be an alias of resolve()

--- async-tree/src/operations/clone.js
+++ async-tree/src/operations/clone.js
@@ -1,17 +1,15 @@
 import * as args from "../utilities/args.js";
-import mapReduce from "./mapReduce.js";
+import resolve from "./resolve.js";
 
 /**
- * Return a copy of the given maplike object
+ * Return a copy of the given maplike object.
+ *
+ * This is currently an alias for resolve(), which ends up cloning a tree.
  *
  * @param {import("../../index.ts").Maplike} maplike
  */
 export default async function clone(maplike) {
   const source = await args.map(maplike, "Tree.clone", { deep: true });
-  const cloned = await mapReduce(
-    source,
-    (x) => x,
-    (results) => results,
-  );
+  const cloned = await resolve(source);
   return cloned;
 }
