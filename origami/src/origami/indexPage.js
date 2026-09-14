ea266524e903c012012ef3503d14bc0296f093cb
Jan Miksovsky <jan@miksovsky.com>
2026-09-14 16:18:31 -0700

    Drop initial-scale

--- origami/src/origami/indexPage.js
+++ origami/src/origami/indexPage.js
@@ -38,7 +38,7 @@ export default async function indexPage(maplike, basePath) {
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="viewport" content="width=device-width">
     <style>
       li {
         margin-bottom: 0.20em;
