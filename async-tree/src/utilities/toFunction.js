8a855072a0d2ae2c1e9d04fd8bf3147680fb276d
Jan Miksovsky <jan@miksovsky.com>
2026-09-16 21:38:11 -0700

    toFunction no longer unpacks

--- async-tree/src/utilities/toFunction.js
+++ async-tree/src/utilities/toFunction.js
@@ -1,6 +1,5 @@
 import from from "../operations/from.js";
 import isMaplike from "../operations/isMaplike.js";
-import isUnpackable from "./isUnpackable.js";
 
 /**
  * Convert the given object to a function.
@@ -14,24 +13,6 @@ export default function toFunction(obj) {
   if (typeof obj === "function") {
     // Return a function as is.
     return obj;
-  } else if (isUnpackable(obj)) {
-    // Extract the contents of the object and convert that to a function.
-    const unpacked = /** @type {any} */ (obj).unpack();
-    if (unpacked instanceof Promise) {
-      return async function (...args) {
-        const fn = toFunction(await unpacked);
-        if (fn === null) {
-          throw new TypeError("unpack() did not return a function");
-        }
-        return fn(...args);
-      };
-    } else {
-      const fn = toFunction(unpacked);
-      if (fn === null) {
-        throw new TypeError("unpack() did not return a function");
-      }
-      return fn;
-    }
   } else if (isMaplike(obj)) {
     // Return a function that invokes the tree's getter.
     const tree = from(obj);
